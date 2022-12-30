e312d6730c5da0b9e09688673546a0772c967941
Tosiek <antonizieciak@gmail.com>
2022-12-30 18:41:10 +0100

    Added glm to project.

---
+++ include/transform.h
@@ -0,0 +1,20 @@
+//
+// Created by tosiek on 30.12.22.
+//
+
+#include <glm/glm.hpp>
+
+#ifndef ARCHIMEDES_TRANSFORM_H
+#define ARCHIMEDES_TRANSFORM_H
+
+
+class Transform {
+
+public:
+    void rotate();
+    void translate();
+    void scale();
+};
+
+
+#endif //ARCHIMEDES_TRANSFORM_H
---
+++ src/transform.cpp
@@ -0,0 +1,5 @@
+//
+// Created by tosiek on 30.12.22.
+//
+
+#include "transform.h"
