05508480db534f9ddc96e03aa5af78663ad0c2e0
Chris-plusplus <72704303+Chris-plusplus@users.noreply.github.com>
2023-04-03 22:38:24 +0200

    net namespace.

--- include/net.hpp
+++ include/net.hpp
@@ -2,8 +2,18 @@
 
 #ifdef _WIN32
 // OS is Windows
-
-
-#endif // _WIN32
+#elif defined unix
+// OS is linux
+#endif
 
 #include <net/ipv4.hpp>
+#include <net/ipv4_network.hpp>
+#include <net/host.hpp>
+#include <net/__net_init.hpp>
+
+namespace arch {
+	/// @brief Namespace of archimedes' networking module.
+	///
+	namespace net {
+	}
+}
