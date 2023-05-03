4abc6f026e01dbf8b3ab0980555d8a458732a973
Chris-plusplus <72704303+Chris-plusplus@users.noreply.github.com>
2023-05-03 18:09:07 +0200

    Aliased WSAPoll() as poll(), because MS forgot to.

--- include/net/includes.hpp
+++ include/net/includes.hpp
@@ -2,6 +2,7 @@
 #include <WinSock2.h>
 #include <WS2tcpip.h>
 #pragma comment(lib, "ws2_32.lib")
+inline auto poll(LPWSAPOLLFD fdArray, ULONG fds, INT timeout) { return WSAPoll(fdArray, fds, timeout); }
 #elif defined unix
 #include <sys/socket.h>
 #include <sys/types.h>
