78bfa6b7c5914abe0d22c493e19646558f830ac5
Chris-plusplus <72704303+Chris-plusplus@users.noreply.github.com>
2023-05-05 22:52:44 +0200

    Include guard for poll() alias.

--- include/net/includes.hpp
+++ include/net/includes.hpp
@@ -2,7 +2,10 @@
 #include <WinSock2.h>
 #include <WS2tcpip.h>
 #pragma comment(lib, "ws2_32.lib")
+#ifndef __ARCH_NET_WIN32_POLL_ALIAS__
+#define __ARCH_NET_WIN32_POLL_ALIAS__
 inline auto poll(LPWSAPOLLFD fdArray, ULONG fds, INT timeout) { return WSAPoll(fdArray, fds, timeout); }
+#endif//__ARCH_NET_WIN32_POLL_ALIAS__
 #elif defined unix
 #include <sys/socket.h>
 #include <sys/types.h>
