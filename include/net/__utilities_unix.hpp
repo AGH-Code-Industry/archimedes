7073d8e2477de064bc24ec6f595861f29aca15ff
Chris-plusplus <72704303+Chris-plusplus@users.noreply.github.com>
2023-07-29 22:23:06 +0200

    Little blunder.

--- include/net/__utilities_unix.hpp
+++ include/net/__utilities_unix.hpp
@@ -14,9 +14,8 @@
 #include <cstring>
 #ifndef __ARCH_NET_UNIX_STD_FUNCTIONS__
 #define __ARCH_NET_UNIX_STD_FUNCTIONS__
-inline int closesocket(int fd) { close(fd); }
+inline int closesocket(int fd) { return close(fd); }
 inline int net_errno(int parent_error) noexcept { return (parent_error == EAI_SYSTEM ? errno : parent_error); }
 inline int net_errno() noexcept { return errno; }
-}
 #endif//__ARCH_NET_UNIX_STD_FUNCTIONS__
 #endif
