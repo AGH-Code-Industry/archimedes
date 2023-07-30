117bab39d9618e0240676e20069165fde7b5f045
Bruno Banaszczyk <bruno.banaszczyk@gmail.com>
2023-07-30 20:15:40 +0200

    implemented unfinished key-function bind system

--- include/input_handler.h
+++ include/input_handler.h
@@ -2,18 +2,25 @@
 #include <GLFW/glfw3.h>
 #include <window.h>
 
+#define BINDED_PAIR_ARRAY_SIZE 30
 #ifndef ARCHIMEDES_INPUT_HANDLER_H
 #define ARCHIMEDES_INPUT_HANDLER_H
 
 namespace arch{
     class InputHandler{
     public:
-        InputHandler(GLFWwindow *_window);        
-
+        InputHandler(GLFWwindow *_window);
+        void bindKey(int keyCode, void (*func)());        
+        struct bindedPair {
+            int keyCode;
+            void (*func_pointer)();
+        };
+        static bindedPair bindedPairArray[BINDED_PAIR_ARRAY_SIZE];
     private:
         GLFWwindow *_window;
-
-        static void keyCallback(GLFWwindow *window, int key, int scancode, int action, int mods);
+        
+        static void handleKeyEvent(GLFWwindow *window, int key, int scancode, int action, int mods);
+        void initializeBindedPairArray();
     };
 }
 
--- src/input_handler.cpp
+++ src/input_handler.cpp
@@ -1,15 +1,40 @@
 #include "input_handler.h"
+
 #include <GLFW/glfw3.h>
+#include <window.h>
+#include <functional>
 
 namespace arch {
+    InputHandler::bindedPair InputHandler::bindedPairArray[BINDED_PAIR_ARRAY_SIZE] {};
     InputHandler::InputHandler(GLFWwindow *_window) {
         this->_window = _window;
-        glfwSetKeyCallback(_window, keyCallback);
+        initializeBindedPairArray();
+        glfwSetKeyCallback(_window, handleKeyEvent);
     }
 
-    void InputHandler::keyCallback(GLFWwindow *window, int key, int scancode, int action, int mods) {
-        if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
-            glfwSetWindowShouldClose(window, true);
+    void InputHandler::bindKey(int keyCode, void (*func)()) {
+        for (int i=0; i<BINDED_PAIR_ARRAY_SIZE; i++) {
+            if (bindedPairArray[i].keyCode == GLFW_KEY_UNKNOWN) {
+                bindedPairArray[i].keyCode = keyCode;
+                bindedPairArray[i].func_pointer = func;
+                break;
+            }
+        }
+    }
+
+    void InputHandler::handleKeyEvent(GLFWwindow *window, int key, int scancode, int action, int mods) {
+        for (int i=0; i<BINDED_PAIR_ARRAY_SIZE; i++) {
+            if (bindedPairArray[i].keyCode == key) {
+                bindedPairArray[i].func_pointer();
+                break;
+            }
+        }
+    }
+
+    void InputHandler::initializeBindedPairArray() {
+        for (int i=0; i<BINDED_PAIR_ARRAY_SIZE; i++) {
+            bindedPairArray[i].keyCode = GLFW_KEY_UNKNOWN;
+            bindedPairArray[i].func_pointer = nullptr;
         }
     }
 }
