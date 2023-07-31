d2e5afcf2546d8c477f1b27ee4793f8416a7e96e
Bruno Banaszczyk <bruno.banaszczyk@gmail.com>
2023-07-31 13:43:47 +0200

    small adjustments

--- include/input_handler.h
+++ include/input_handler.h
@@ -11,12 +11,14 @@ namespace arch{
     class InputHandler{
     public:
         InputHandler(GLFWwindow *_window);
-        void bindKey(int keyCode, std::function<void()> func);        
+        void bindKey(int keyCode, std::function<void()> func); 
+        void bindAll();       
 
     private:
         static GLFWwindow *_window;
         static std::unordered_map<int, std::function<void()>> bindedTable;
         static void closeWindow();
+        static void maximizeWindow();
         static void handleKeyEvent(GLFWwindow *window, int key, int scancode, int action, int mods);
     };
 }
--- src/input_handler.cpp
+++ src/input_handler.cpp
@@ -6,7 +6,12 @@ namespace arch {
     InputHandler::InputHandler(GLFWwindow *_window) {
         this->_window = _window;
         glfwSetKeyCallback(_window, handleKeyEvent);
+        bindAll();
+    }
+
+    void InputHandler::bindAll() {
         bindKey(GLFW_KEY_ESCAPE, &closeWindow);
+        bindKey(GLFW_KEY_F, &maximizeWindow);
     }
 
     void InputHandler::bindKey(int keyCode, std::function<void()> func) {
@@ -15,6 +20,10 @@ namespace arch {
 
     void InputHandler::closeWindow() {
         glfwSetWindowShouldClose(_window, true);
+    }
+
+    void InputHandler::maximizeWindow() {
+        glfwMaximizeWindow(_window);
     }
 
     void InputHandler::handleKeyEvent(GLFWwindow *window, int key, int scancode, int action, int mods) {
