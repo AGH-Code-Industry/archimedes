372799b93f2b7900d1ab09a25ee2cbc68964125d
Chris-plusplus <72704303+Chris-plusplus@users.noreply.github.com>
2023-08-04 16:13:03 +0200

    Removed typedefs and moved stream operator inside class.

--- include/net/ipv4.hpp
+++ include/net/ipv4.hpp
@@ -9,17 +9,14 @@ namespace arch::net {
 ///
 class IPv4 {
 public:
-	/// @brief Type of octet.
-	///
-	using octet_type = unsigned char;
 	/// @brief Type of binary value.
 	///
 	using binary_type = uint32_t;
 	/// @brief Union type of data
 	///
 	union data_type {
-		octet_type octets[4];
-		binary_type binary;
+		unsigned char octets[4];
+		uint32_t binary;
 	};
 
 	/// @brief Example loopback address.
@@ -31,30 +28,38 @@ public:
 	IPv4();
 	/// @brief Decimal value constructor.
 	/// @param decimal - decimal form of IPv4 address.
-	IPv4(std::string_view decimal);
+	IPv4(const std::string& decimal);
 	/// @brief Octet array constructor.
 	/// @param octets - array of 4 octets of IPv4 address.
-	IPv4(octet_type octets[4]);
+	IPv4(unsigned char octets[4]);
 	/// @brief Octets constructor.
 	/// @param octet_0 - first octet.
-	/// @param octet_0 - second octet.
-	/// @param octet_0 - third octet.
-	/// @param octet_0 - forth octet.
-	IPv4(octet_type octet_0, octet_type octet_1, octet_type octet_2,
-		 octet_type octet_3);
+	/// @param octet_1 - second octet.
+	/// @param octet_2 - third octet.
+	/// @param octet_3 - forth octet.
+	IPv4(unsigned char octet_0, unsigned char octet_1, unsigned char octet_2,
+		 unsigned char octet_3);
 	/// @brief Binary value constructor
 	/// @param binary - binary value of address.
-	explicit IPv4(binary_type binary);
+	explicit IPv4(uint32_t binary);
 	/// @brief in_addr constructor.
 	/// @param addr - in_addr structure to convert from.
 	IPv4(in_addr addr);
+
+
+
+	/// @brief ostream insertion operator.
+	/// @param os - ostream.
+	/// @param ip - ip to insert.
+	///
+	friend std::ostream& operator<<(std::ostream& os, const IPv4& ip);
 
 	/// @brief Gets decimal form of address.
 	///
 	std::string str() const;
 	/// @brief Sets decimal value of address.
 	/// @param decimal - decimal form of IPv4 address.
-	void str(std::string_view decimal);
+	void str(const std::string& decimal);
 	/// @brief Converts to in_addr.
 	///
 	operator in_addr() const;
@@ -67,11 +72,11 @@ public:
 	void data(data_type new_data);
 	/// @brief Sets binary data of address.
 	/// @param new_data - new data of address.
-	void data(binary_type new_data);
+	void data(uint32_t new_data);
 	/// @brief Sets octet data of address.
 	/// @param octet - number of octet to set.
 	/// @param new_octet - new value of octet.
-	void data(size_t octet, octet_type new_octet);
+	void data(size_t octet, unsigned char new_octet);
 
 	/// @brief Preincrement operator.
 	///
@@ -97,10 +102,4 @@ public:
 protected:
 	data_type _data;
 };
-
-/// @brief ostream insertion operator.
-/// @param os - ostream.
-/// @param ip - ip to insert.
-///
-std::ostream& operator<<(std::ostream& os, const IPv4& ip);
 }  // namespace arch::net
