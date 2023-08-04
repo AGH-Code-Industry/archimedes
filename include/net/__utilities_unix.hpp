298c28fdb34f7dd2fe146bd29901b114d37f519e
Chris-plusplus <72704303+Chris-plusplus@users.noreply.github.com>
2023-08-04 16:10:04 +0200

    Changed include guards to #pragma once.

--- include/net/__utilities_unix.hpp
+++ include/net/__utilities_unix.hpp
@@ -1,10 +1,14 @@
+#pragma once
 #ifdef unix // no errors in IDE
+
 #ifndef INVALID_SOCKET
 #define INVALID_SOCKET -1
 #endif
+
 #ifndef SOCKET_ERROR
 #define SOCKET_ERROR -1
 #endif
+
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <arpa/inet.h>
@@ -13,10 +17,9 @@
 #include <unistd.h>
 #include <cstring>
 #include <cerrno>
-#ifndef __ARCH_NET_UNIX_STD_FUNCTIONS__
-#define __ARCH_NET_UNIX_STD_FUNCTIONS__
+
 inline int closesocket(int fd) { return close(fd); }
 inline int net_errno(int parent_error) noexcept { return (parent_error == EAI_SYSTEM ? errno : parent_error); }
 inline int net_errno() noexcept { return errno; }
-#endif//__ARCH_NET_UNIX_STD_FUNCTIONS__
+
 #endif
--- include/net/__utilities_windows.hpp
+++ include/net/__utilities_windows.hpp
@@ -1,12 +1,15 @@
+#pragma once
 #ifdef WIN32 // no errors in IDE
+
 #define NOMINMAX
+
 #include <WinSock2.h>
 #include <WS2tcpip.h>
+
 #pragma comment(lib, "ws2_32.lib")
-#ifndef __ARCH_NET_WIN32_STD_FUNCTIONS__
-#define __ARCH_NET_WIN32_STD_FUNCTIONS__
+
 inline auto poll(LPWSAPOLLFD fdArray, ULONG fds, INT timeout) { return WSAPoll(fdArray, fds, timeout); }
 inline int net_errno(int parent_error) noexcept { return WSAGetLastError(); }
 inline int net_errno() noexcept { return WSAGetLastError(); }
-#endif//__ARCH_NET_WIN32_STD_FUNCTIONS__
+
 #endif
