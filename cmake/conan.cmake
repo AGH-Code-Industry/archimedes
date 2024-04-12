316eec2b16f3cad5244c970714eae7c82cef0a79
Patryk Twardosz <patryk280502@gmail.com>
2024-04-13 01:19:20 +0200

    Fix CI

--- cmake/conan.cmake
+++ cmake/conan.cmake
@@ -23,7 +23,7 @@ if(ARCHIMEDES_FORCE_CONAN_INSTALL OR NOT EXISTS "${PROJECT_SOURCE_DIR}/cmake/con
 	endif()
 
 	execute_process(
-		COMMAND "conan profile detect --force"
+		COMMAND "conan" "profile" "detect" "--force"
 	) 
 
 	file(MAKE_DIRECTORY "${PROJECT_SOURCE_DIR}/cmake/conan_files/${ARCHIMEDES_BUILD_TYPE}/")
