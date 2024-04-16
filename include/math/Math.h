c9bcb49ff1ea7d6ba29b8b511aff8b30dcd0c1da
Twarug <patryk280502@gmail.com>
2024-04-16 20:49:49 +0200

    Add basic ArchMath

---
+++ include/math/Math.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include <glm/mat2x2.hpp>
+#include <glm/mat3x3.hpp>
+#include <glm/mat4x4.hpp>
+#include <glm/vec2.hpp>
+#include <glm/vec3.hpp>
+#include <glm/vec4.hpp>
+
+namespace arch::math {
+
+using Byte = std::byte;
+
+using i8 = int8_t;
+using i16 = int16_t;
+using i32 = int32_t;
+using i64 = int64_t;
+
+using u8 = uint8_t;
+using u16 = uint16_t;
+using u32 = uint32_t;
+using u64 = uint64_t;
+
+using f32 = float_t;
+using f64 = double_t;
+
+using float2 = glm::vec2;
+using float3 = glm::vec3;
+using float4 = glm::vec4;
+
+using int2 = glm::ivec2;
+using int3 = glm::ivec3;
+using int4 = glm::ivec4;
+
+using uint2 = glm::uvec2;
+using uint3 = glm::uvec3;
+using uint4 = glm::uvec4;
+
+using Mat2x2 = glm::mat2;
+using Mat3x3 = glm::mat3;
+using Mat4x4 = glm::mat4;
+
+using Color = glm::vec4;
+
+} // namespace arch::math
--- src/Transform.cpp
+++ src/Transform.cpp
@@ -1,59 +1,61 @@
 #include "Transform.h"
 
+#include <glm/gtc/matrix_transform.hpp>
+
 namespace arch {
 
-std::pair<Quaternion, Quaternion> Transform::rotate(float angle, float x, float y, float z) {
+std::pair<Quaternion, Quaternion> Transform::rotate(f32 angle, f32 x, f32 y, f32 z) {
 	// normalize x,y,z
-	auto norm = static_cast<float>(sqrt(pow(x, 2) + pow(y, 2) + pow(z, 2)));
+	auto norm = static_cast<f32>(sqrt(pow(x, 2) + pow(y, 2) + pow(z, 2)));
 	x /= norm;
 	y /= norm;
 	z /= norm;
 
 	// fix angle
 	angle /= 2;
-	const double pi = std::acos(-1);
-	angle *= static_cast<float>(pi / 180.0);
+	const f64 pi = std::acos(-1);
+	angle *= static_cast<f32>(pi / 180.0);
 
 	// create rotation quaternions
-	float a = x * std::sin(angle);
-	float b = y * std::sin(angle);
-	float c = z * std::sin(angle);
-	glm::vec3 imaginaryPart = glm::vec3(a, b, c);
-	glm::vec4 q = glm::vec4(imaginaryPart, std::cos(angle));
-	glm::vec4 q1 = glm::vec4(-imaginaryPart, std::cos(angle));
+	f32 a = x * std::sin(angle);
+	f32 b = y * std::sin(angle);
+	f32 c = z * std::sin(angle);
+	float3 imaginaryPart = float3(a, b, c);
+	float4 q = float4(imaginaryPart, std::cos(angle));
+	float4 q1 = float4(-imaginaryPart, std::cos(angle));
 
-	return {q, q1};
+	return { q, q1 };
 }
 
-std::pair<Quaternion, Quaternion> Transform::rotate(float angle, glm::vec3 rotationAxis) {
+std::pair<Quaternion, Quaternion> Transform::rotate(f32 angle, float3 rotationAxis) {
 	return rotate(angle, rotationAxis.x, rotationAxis.y, rotationAxis.z);
 }
 
-glm::mat4 Transform::translate(glm::vec3 translationVector) {
-	glm::mat4 translationMatrix = glm::translate(glm::mat4(1.0f), translationVector);
+Mat4x4 Transform::translate(float3 translationVector) {
+	Mat4x4 translationMatrix = glm::translate(Mat4x4(1.0f), translationVector);
 	return translationMatrix;
 }
 
-glm::mat4 Transform::translate(float x, float y, float z) {
-	glm::vec3 translationVector = glm::vec3(x, y, z);
+Mat4x4 Transform::translate(f32 x, f32 y, f32 z) {
+	float3 translationVector = float3(x, y, z);
 
 	return translate(translationVector);
 }
 
-glm::mat4 Transform::scale(glm::vec3 scalingVector) {
-	glm::mat4 scalingMatrix = glm::scale(glm::mat4(1.0f), scalingVector);
+Mat4x4 Transform::scale(float3 scalingVector) {
+	Mat4x4 scalingMatrix = glm::scale(Mat4x4(1.0f), scalingVector);
 
 	return scalingMatrix;
 }
 
-glm::mat4 Transform::scale(float x, float y, float z) {
-	glm::vec3 scalingVector = glm::vec3(x, y, z);
+Mat4x4 Transform::scale(f32 x, f32 y, f32 z) {
+	float3 scalingVector = float3(x, y, z);
 
 	return scale(scalingVector);
 }
 
-glm::mat4 Transform::scale(float a) {
-	auto scalingVector = glm::vec3(a);
+Mat4x4 Transform::scale(f32 a) {
+	auto scalingVector = float3(a);
 
 	return scale(scalingVector);
 }
