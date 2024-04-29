ae32cb527345957eccc79b0c568d90758be23c39
Chris-plusplus <72704303+Chris-plusplus@users.noreply.github.com>
2024-04-29 20:07:39 +0200

    Fix long build time

--- cmake/library.cmake
+++ cmake/library.cmake
@@ -4,11 +4,10 @@ include("${PROJECT_SOURCE_DIR}/cmake/conan.cmake")
 
 add_library(${PROJECT_NAME})
 
-include_directories(include)
-
 # find source files
 file(GLOB_RECURSE ARCHIMEDES_SOURCE src/**.cpp)
-target_sources(${PROJECT_NAME} PUBLIC ${ARCHIMEDES_SOURCE})
+target_sources(${PROJECT_NAME} PRIVATE ${ARCHIMEDES_SOURCE})
+target_include_directories(${PROJECT_NAME} PUBLIC include)
 
 # link conan libraries
 target_link_libraries(${PROJECT_NAME} PUBLIC ${ARCHIMEDES_LIBRARIES})
