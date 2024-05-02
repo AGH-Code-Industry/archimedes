e660079d9f694f558a0e5fe5bd8ff145e171cad4
Twarug <patryk280502@gmail.com>
2024-05-02 18:29:48 +0200

    Improve createRef function

--- include/Ref.h
+++ include/Ref.h
@@ -8,8 +8,15 @@ template<typename T>
 using Ref = std::shared_ptr<T>;
 
 template<typename T, typename... Args>
+requires std::constructible_from<T, Args...>
 constexpr Ref<T> createRef(Args&&... args) {
 	return std::make_shared<T>(std::forward<Args>(args)...);
+}
+
+template<typename T>
+requires std::derived_from<T, std::enable_shared_from_this<T>>
+constexpr Ref<T> createRef(T* ptr) {
+	return ptr->shared_from_this();
 }
 
 } // namespace arch
