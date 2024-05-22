6d01f8bfc4d1fcfd1da9e30deb39d778cb8ba069
Twarug <patryk280502@gmail.com>
2024-05-22 12:10:19 +0200

    Make CMake rebuild unnecessary after creating/removing a file

--- cmake/library.cmake
+++ cmake/library.cmake
@@ -7,7 +7,7 @@ add_library(${PROJECT_NAME})
 include_directories(include)
 
 # find source files
-file(GLOB_RECURSE ARCHIMEDES_SOURCE src/**.cpp)
+file(GLOB_RECURSE ARCHIMEDES_SOURCE CONFIGURE_DEPENDS src/**.cpp)
 target_sources(${PROJECT_NAME} PUBLIC ${ARCHIMEDES_SOURCE})
 
 # link conan libraries
