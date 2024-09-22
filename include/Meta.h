9ccff0b2886aaa2fe0784dc91fb78533a5536fb7
Chris-plusplus <72704303+Chris-plusplus@users.noreply.github.com>
2024-09-23 00:02:39 +0200

    removed meta.h

--- include/Meta.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "meta/Typeof.hpp"
-
-#include "meta/NameOfType.h"
-#include "meta/Rtti.h"
-#include "meta/ThisT.h"
--- include/meta.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "meta/Typeof.hpp"
-
-#include "meta/baseUtils.h"
-#include "meta/nameOfType.h"
-#include "meta/rtti.h"
