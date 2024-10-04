896a5efa921a99b750fb7c270d51e44b719f0f17
Chris-plusplus <72704303+Chris-plusplus@users.noreply.github.com>
2024-10-04 18:38:50 +0200

    Changes not_fn(hasNull) to hasNotNull

--- include/ecs/CommonComponentPool.hpp
+++ include/ecs/CommonComponentPool.hpp
@@ -7,7 +7,7 @@ namespace arch::ecs::_details { // NOLINT
 template<class E>
 auto CommonComponentPool<E>::_entitiesForView() const noexcept {
 	// entites with version != null are valid
-	return std::views::filter(_dense, std::not_fn(_details::EntityTraits<E>::Version::hasNull));
+	return std::views::filter(_dense, _details::EntityTraits<E>::Version::hasNotNull);
 }
 
 } // namespace arch::ecs::_details
