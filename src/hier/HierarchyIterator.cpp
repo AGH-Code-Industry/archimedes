ca8cf69b4c7f950548f546d9dc0c39e7e0d67af7
Chris-plusplus <72704303+Chris-plusplus@users.noreply.github.com>
2024-11-14 00:07:40 +0100

    refactor HierarchyIterator ++ and --

--- src/hier/HierarchyIterator.cpp
+++ src/hier/HierarchyIterator.cpp
@@ -7,7 +7,7 @@
 // cast ChildNode* x to HierarchyNode*, used only if !SENT(x)
 #define NODE(x) (static_cast<const ::arch::hier::HierarchyNode*>((x)))
 
-namespace arch::hier {
+namespace arch::hier::_details {
 
 HierarchyIterator::HierarchyIterator(const ChildNode* node) noexcept: _current{ node } {}
 
@@ -20,15 +20,17 @@ HierarchyIterator& HierarchyIterator::operator++() noexcept {
 	if (!SENT(_current)) {
 		auto currentHierNode = NODE(_current);
 		if (currentHierNode->first != nullptr) {
+			// jump to first child
 			_current = currentHierNode->first;
-		} else if (!SENT(_current->next) || ROOT(currentHierNode->parent)) {
-			_current = _current->next;
-		} else {
+			return *this;
+		} else if (SENT(_current->next) && !ROOT(currentHierNode->parent)) {
+			// last of non-root parent, jump to next
 			_current = currentHierNode->parent->next;
+			return *this;
 		}
-	} else {
-		_current = _current->next;
 	}
+
+	_current = _current->next;
 	return *this;
 }
 
@@ -39,15 +41,22 @@ HierarchyIterator HierarchyIterator::operator++(int) noexcept {
 }
 
 HierarchyIterator& HierarchyIterator::operator--() noexcept {
-	if (SENT(_current) || !SENT(_current->prev)) {
-		auto prevSibling = NODE(_current->prev);
-		_current = prevSibling->count != 0 ? prevSibling->last : prevSibling;
-	} else if (auto currentHierNode = NODE(_current); !ROOT(currentHierNode->parent)) {
-		_current = currentHierNode->parent;
-	} else {
-		_current = _current->prev;
+	if (!SENT(_current)) {
+		if (!SENT(_current->prev)) {
+			auto currentPrevLast = NODE(_current->prev)->last;
+			if (currentPrevLast != nullptr) {
+				// prev has children, jump to last
+				_current = currentPrevLast;
+				return *this;
+			}
+		} else if (auto currentParent = NODE(_current)->parent; !ROOT(currentParent)) {
+			// last children of non-root parent, jump to parent
+			_current = currentParent;
+			return *this;
+		}
 	}
 
+	_current = _current->prev;
 	return *this;
 }
 
@@ -57,7 +66,7 @@ HierarchyIterator HierarchyIterator::operator--(int) noexcept {
 	return temp;
 }
 
-} // namespace arch::hier
+} // namespace arch::hier::_details
 
 #undef SENT
 #undef ROOT
