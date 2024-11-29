400ab62dc667dacec7f78d1cbfcaa2ea4bedbc1e
Tosiekdev <antonizieciak@gmail.com>
2024-11-29 22:35:27 +0100

    Simple update in physics system.

--- src/physics/System.cpp
+++ src/physics/System.cpp
@@ -1,12 +1,31 @@
+#include "ecs/View.h"
 #include "physics/System.h"
+#include "physics/components/Force.h"
+#include "physics/components/Mass.h"
+#include "physics/components/Velocity.h"
 
 namespace arch::physics {
+
+void System::setDomain(ecs::Domain& domain) {
+	_domain = &domain;
+}
 
 void System::update() {
 	if (!_domain) {
 		throw std::runtime_error("System does not have a domain");
 	}
-	// auto viewPhysicsComponents = _domain->view<>;
+	auto viewPhysicsComponents = _domain->view<const Force, const Mass, Velocity>();
+	for (const ecs::Entity entity : viewPhysicsComponents) {
+		auto compTuple = viewPhysicsComponents.get(entity);
+		auto& f = std::get<0>(compTuple);
+		auto& m = std::get<1>(compTuple);
+		auto& v = std::get<2>(compTuple);
+
+		auto a = f.value / m.mass;
+
+		static constexpr float deltaTime = 1.0f;
+		v.value += a * deltaTime;
+	}
 }
 
 }
--- tests/physics/System.cpp
+++ tests/physics/System.cpp
@@ -1,7 +1,29 @@
 #include <gtest/gtest.h>
+
+#include <Ecs.h>
 #include <physics/System.h>
+#include <physics/components/Force.h>
+#include <physics/components/Mass.h>
+#include <physics/components/Velocity.h>
 
 TEST(Physics, TestUpdate) {
-	using PhysicsSystem = arch::physics::System;
-	EXPECT_THROW(PhysicsSystem::update(), std::runtime_error);
+	namespace ecs = arch::ecs;
+	namespace phy = arch::physics;
+	namespace math = arch::math;
+	ecs::Domain domain;
+
+	auto e0 = domain.newEntity();
+
+	domain.addComponent<phy::Force>(e0);
+	domain.addComponent<phy::Mass>(e0);
+	domain.addComponent<phy::Velocity>(e0);
+
+	domain.getComponent<phy::Force>(e0).value = math::float2(1.0f, 1.0f);
+	domain.getComponent<phy::Mass>(e0).mass = 1.0f;
+
+	phy::System::setDomain(domain);
+
+	phy::System::update();
+
+	EXPECT_EQ(domain.getComponent<phy::Velocity>(e0).value, math::float2(1.0f, 1.0f));
 }
