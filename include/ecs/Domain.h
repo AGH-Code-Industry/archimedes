75ac0fd35a0e6bc7996efc9ce33ad6938ab4c99c
Chris-plusplus <72704303+Chris-plusplus@users.noreply.github.com>
2025-02-13 17:38:11 +0100

    remove <iostream> from Domain & View

--- include/ecs/Domain.h
+++ include/ecs/Domain.h
@@ -1,7 +1,6 @@
 #pragma once
 
 #include <concepts>
-#include <iostream>
 #include <unordered_map>
 
 #include "ComponentPool.h"
--- include/ecs/View.h
+++ include/ecs/View.h
@@ -1,6 +1,5 @@
 #pragma once
 
-#include <iostream>
 #include <ranges>
 #include <type_traits>
 
