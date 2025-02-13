3883205034509a6db326a2c74a6281134d6b61fb
Chris-plusplus <72704303+Chris-plusplus@users.noreply.github.com>
2025-02-13 17:47:25 +0100

    Update Scene.h

--- include/Scene.h
+++ include/Scene.h
@@ -1,9 +1,9 @@
 #pragma once
-
+#include "scene/Components.h"
+#include "scene/SceneManager.h"
 #include <scene/ChildrenIterator.h>
-#include <scene/HierarchyIterator.h>
-//
 #include <scene/Entity.h>
+#include <scene/HierarchyIterator.h>
 #include <scene/Scene.h>
 
 namespace arch {
