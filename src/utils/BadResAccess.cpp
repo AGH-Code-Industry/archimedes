127d2e0493227f7a3f277a976c47ba6728f588b9
Chris-plusplus <72704303+Chris-plusplus@users.noreply.github.com>
2025-02-13 16:08:19 +0100

    Create BadResAccess.cpp

---
+++ src/utils/BadResAccess.cpp
@@ -0,0 +1,4 @@
+#include <utils/res/BadResAccess.h>
+
+arch::utils::BadResAccess::BadResAccess(std::string_view msg, const std::source_location& location) noexcept:
+	Exception("BadResAccess", msg.data(), location) {}
