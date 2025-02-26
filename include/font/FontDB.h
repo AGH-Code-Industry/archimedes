0e0b6447de70479e904da765050db5d232d788a8
Chris-plusplus <72704303+Chris-plusplus@users.noreply.github.com>
2025-02-26 01:29:17 +0100

    added FontDB class

---
+++ include/font/FontDB.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <array>
+#include <memory>
+#include <string>
+#include <string_view>
+#include <unordered_map>
+
+#include "FontStyle.h"
+#include "utils/OptRef.h"
+#include <utils/StringViewComparator.h>
+#include <utils/StringViewHasher.h>
+
+namespace arch::font {
+
+class Font;
+
+class FontDB {
+	using MapType = std::unordered_map<std::string, Font, utils::StringViewHasher, utils::StringViewComparator>;
+
+public:
+
+	~FontDB() noexcept;
+
+	static FontDB& get() noexcept;
+
+	bool exists(std::string_view familyName) const noexcept;
+	OptRef<Font> get(std::string_view familyName) noexcept;
+
+	MapType::const_iterator begin() const noexcept;
+	MapType::const_iterator end() const noexcept;
+
+private:
+
+	friend class Font;
+
+	FontDB() noexcept;
+
+	static std::unique_ptr<FontDB> _singleton;
+
+	void _findAndAddFonts() noexcept;
+
+	MapType _fonts;
+	char* _pimpl;
+};
+
+} // namespace arch::font
---
+++ src/font/FontDB.cpp
@@ -0,0 +1,205 @@
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <unordered_set>
+#include <vector>
+
+#include <Logger.h>
+#include <font/Font.h>
+#include <font/FontDB.h>
+#include <freetype2/ft2build.h>
+#include <utils/Assert.h>
+#include <windows.h>
+#include FT_FREETYPE_H
+
+namespace fs = std::filesystem;
+namespace chr = std::chrono;
+using clk = chr::high_resolution_clock;
+
+namespace arch::font {
+
+std::unique_ptr<FontDB> FontDB::_singleton;
+
+#if ARCHIMEDES_WINDOWS
+void findAndAddFontsWindows(
+	std::unordered_map<std::string, Font, utils::StringViewHasher, utils::StringViewComparator>& fonts,
+	char* pimpl
+) noexcept {
+	FT_Library ft = (FT_Library)pimpl;
+	auto time = decltype(clk::now() - clk::now())();
+	for (const auto hRootKey : { HKEY_LOCAL_MACHINE, HKEY_CURRENT_USER }) {
+		constexpr LPCSTR subKey = "SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\Fonts";
+
+		HKEY hKey;
+		if (RegOpenKeyEx(hRootKey, subKey, 0, KEY_READ, &hKey) != ERROR_SUCCESS) {
+			arch::Logger::error("Cannot open registry key: {}", subKey);
+			continue;
+		}
+
+		char valueName[256]{};
+		DWORD valueNameSize = sizeof(valueName);
+
+		unsigned char valueData[1'024]{};
+		DWORD valueDataSize = sizeof(valueData);
+
+		DWORD type;
+		for (DWORD i = 0;; ++i, valueNameSize = sizeof(valueName), valueDataSize = sizeof(valueData)) {
+			auto result = RegEnumValue(hKey, i, valueName, &valueNameSize, NULL, &type, valueData, &valueDataSize);
+			if (result == ERROR_NO_MORE_ITEMS) {
+				break;
+			} else if (result == ERROR_SUCCESS && type == REG_SZ) {
+				// we have path to font file
+
+				auto path = std::string((char*)valueData);
+				if (!fs::path(path).has_parent_path()) {
+					path = "C:/Windows/Fonts/" + path;
+				}
+
+				// TODO: wczytywanie z pliku
+				// auto start = clk::now();
+				// std::vector<char> fontData;
+				// if (fs::exists(path)) {
+				//	// std::cout << std::format("reading {}\n", path);
+				//	auto file = std::ifstream(path, std::ios::in | std::ios::binary | std::ios::ate);
+				//	auto fileSize = file.tellg();
+				//	file.seekg(0);
+
+				//	fontData.resize(fileSize);
+				//	file.read(fontData.data(), fileSize);
+				//}
+				// auto end = clk::now();
+				// time += (end - start);
+
+				FT_Face face{};
+				FT_Long faceCount = 0;
+				FT_Long i = 0;
+
+				do {
+					if (/*FT_New_Memory_Face(ft, (FT_Byte*)fontData.data(), fontData.size(), i, &face)*/
+						FT_New_Face(ft, path.c_str(), i, &face) == 0) {
+						if (faceCount == 0) {
+							faceCount = face->num_faces;
+						}
+
+						// currently supports only bold & italic
+						auto styleFlags = face->style_flags & 0b11;
+						// std::cout << std::format("'{}'\n", face->style_name);
+
+						// build set of styles
+						std::stringstream stylesStream;
+						stylesStream.str(face->style_name);
+						std::unordered_set<std::string, utils::StringViewHasher, utils::StringViewComparator> stylesSet;
+						while (!stylesStream.eof()) {
+							std::string subStyle;
+							stylesStream >> subStyle;
+							stylesSet.insert(std::move(subStyle));
+						}
+
+						if (stylesSet.size() > 2) {
+							// currently supports only bold & itali
+							continue;
+						}
+
+						unsigned int stylesMask = 0;
+						if (stylesSet.contains("Regular")) {
+							stylesMask |= FontStyle::regular;
+						} else {
+							bool isBold = stylesSet.contains("Bold");
+							bool isItalic = stylesSet.contains("Italic");
+
+							if (isBold && stylesSet.size() == 1) {
+								stylesMask |= FontStyle::bold;
+							} else if (isItalic && stylesSet.size() == 1) {
+								stylesMask |= FontStyle::italic;
+							} else if (isBold && isItalic) {
+								stylesMask |= FontStyle::bold | FontStyle::italic;
+							}
+						}
+
+						styleFlags = stylesMask;
+
+						if (styleFlags != 0) {
+							styleFlags >>= 1;
+							auto foundFont = fonts.find(face->family_name);
+
+							// set familyName
+							if (foundFont == fonts.end()) {
+								auto&& [it, ignore] = fonts.insert({ face->family_name, {} });
+
+								it->second._familyName = it->first;
+								foundFont = it;
+							} else if (foundFont->second.familyName().empty()) {
+								foundFont->second._familyName = foundFont->first;
+							}
+
+							auto&& font = foundFont->second;
+
+							font._paths[styleFlags] = path;
+
+							if (styleFlags & FT_STYLE_FLAG_ITALIC) {
+								font._styles |= FontStyle::italic;
+							}
+							if (styleFlags & FT_STYLE_FLAG_BOLD) {
+								font._styles |= FontStyle::bold;
+							}
+							if (!styleFlags) {
+								font._styles |= FontStyle::regular;
+							}
+						}
+					}
+				} while (++i < faceCount);
+			}
+		}
+	}
+	std::cout << chr::duration_cast<chr::milliseconds>(time) << '\n';
+}
+#elif ARCHIMEDES_LINUX
+void findAndAddFontsLinux(
+	std::unordered_map<std::string, Font, StringViewHasher, StringViewComparator>& fonts,
+	char* pimpl
+) noexcept {}
+#endif
+
+void FontDB::_findAndAddFonts() noexcept {
+#if ARCHIMEDES_WINDOWS
+	findAndAddFontsWindows(_fonts, _pimpl);
+#endif
+}
+
+FontDB::FontDB() noexcept {
+	FT_Init_FreeType(reinterpret_cast<FT_Library*>(&_pimpl));
+	_findAndAddFonts();
+}
+
+FontDB::~FontDB() noexcept {
+	FT_Done_FreeType(reinterpret_cast<FT_Library>(_pimpl));
+}
+
+FontDB& FontDB::get() noexcept {
+	if (!_singleton) {
+		_singleton = std::unique_ptr<FontDB>(new FontDB());
+	}
+	return *_singleton;
+}
+
+bool FontDB::exists(std::string_view familyName) const noexcept {
+	return _fonts.contains(familyName);
+}
+
+OptRef<Font> FontDB::get(std::string_view familyName) noexcept {
+	auto found = _fonts.find(familyName);
+	if (found != _fonts.end()) {
+		return found->second;
+	}
+	return std::nullopt;
+}
+
+FontDB::MapType::const_iterator FontDB::begin() const noexcept {
+	return _fonts.cbegin();
+}
+
+FontDB::MapType::const_iterator FontDB::end() const noexcept {
+	return _fonts.cend();
+}
+
+} // namespace arch::font
