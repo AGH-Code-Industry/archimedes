a1e12ad076d640016ed29a2e4dfe2993c58382c8
Twarug <patryk@twardosz.dev>
2025-03-01 10:36:18 +0100

    Revert back original NvrhiRendererTestApp

--- archimedes_bin/examples/NvrhiRendererTestApp.h
+++ archimedes_bin/examples/NvrhiRendererTestApp.h
@@ -3,8 +3,6 @@
 #include <Ecs.h>
 #include <Engine.h>
 #include <Scene.h>
-#define STB_IMAGE_IMPLEMENTATION
-#include <stb_image.h>
 
 using namespace arch;
 
@@ -37,35 +35,6 @@ class NvrhiRendererTestApp: public Application {
 		// material->SetFloat3("_pos", glm::vec3(0.5f, 0.5f, 0.5f));
 		// material->SetColor("_color", glm::vec3(1.0f, 0.0f, 0.0f));
 
-		stbi_set_flip_vertically_on_load(true);
-		int atlasWidth, atlasHeight, nChannels;
-		unsigned char* data = stbi_load("atlas.png", &atlasWidth, &atlasHeight, &nChannels, STBI_rgb_alpha);
-		assert(data);
-
-		auto atlasPixels = std::vector<Color>(atlasWidth * atlasHeight);
-		for (u32 i = 0; i != atlasPixels.size(); ++i) {
-			atlasPixels[i].r = (float)data[4 * i + 0] / 255.f;
-			atlasPixels[i].g = (float)data[4 * i + 1] / 255.f;
-			atlasPixels[i].b = (float)data[4 * i + 2] / 255.f;
-			atlasPixels[i].a = (float)data[4 * i + 3] / 255.f;
-		}
-
-		stbi_image_free(data);
-
-		// std::vector<Vertex> atlasVertices{
-		//	{ { 0.f, 0.f, 0.f }, { 0.f, 0.f } },
-		//	{ { 1.f, 0.f, 0.f }, { 1.f, 0.f } },
-		//	{ { 0.f, 1.f, 0.f }, { 0.f, 1.f } },
-		//	//{ { 1.f, 1.f, 0.f }, { 1.f, 1.f } },
-		// };
-		std::vector<Vertex> vertices2{
-			{  { 0.f, 0.f, 0.f }, { 1.f, 1.f } },
-			{  { 0.f, 1.f, 0.f }, { 1.f, 0.f } },
-			{ { 1.f, 0.0f, 0.f }, { 0.f, 1.f } },
-			{ { 1.f, 1.0f, 0.f }, { 0.f, 0.f } },
-		};
-		std::vector<u32> atlasIndices{ 0, 1, 2, 1, 3, 2 };
-
 		Color pixels[] = {
 			Color{ 1, .5, 1, 1 }
 		};
@@ -73,93 +42,76 @@ class NvrhiRendererTestApp: public Application {
 		Ref<gfx::Renderer> renderer = gfx::Renderer::getCurrent();
 
 		Ref<gfx::texture::Texture> texture = renderer->getTextureManager()->createTexture2D(1, 1, pixels);
-		auto atlasTexture = renderer->getTextureManager()->createTexture2D(atlasWidth, atlasHeight, atlasPixels.data());
 
-		auto pipeline = renderer->getPipelineManager()->create({
-			.vertexShaderPath = "shaders/vertex_default.glsl",
-			.fragmentShaderPath = "shaders/fragment_default2.glsl",
-			.textures = { texture },
-			.buffers = {},
-		});
-		auto pipeline2 = renderer->getPipelineManager()->create({
-			.vertexShaderPath = "shaders/vertex_default.glsl",
-			.fragmentShaderPath = "shaders/fragment_default2.glsl",
-			.textures = {},
-			.buffers = {},
-		});
-
-		Ref<asset::mesh::Mesh> atlasMesh = asset::mesh::Mesh::create<Vertex>(vertices2, atlasIndices);
-		auto atlasPipeline = renderer->getPipelineManager()->create({
-			.vertexShaderPath = "shaders/vertex_default.glsl",
-			.fragmentShaderPath = "shaders/fragment_atlas.glsl",
-			.textures = { atlasTexture },
-			.buffers = {},
-		});
+		auto pipeline = renderer->getPipelineManager()->create(
+			{
+				.vertexShaderPath = "shaders/vertex_default.glsl",
+				.fragmentShaderPath = "shaders/fragment_default.glsl",
+				.textures = { texture },
+				.buffers = {},
+			}
+		);
+		auto pipeline2 = renderer->getPipelineManager()->create(
+			{
+				.vertexShaderPath = "shaders/vertex_default.glsl",
+				.fragmentShaderPath = "shaders/fragment_default2.glsl",
+				.textures = {},
+				.buffers = {},
+			}
+		);
 
 		Ref<asset::mesh::Mesh> mesh = asset::mesh::Mesh::create<Vertex>(vertices, indices);
 
 		{
-			auto e = testScene->newEntity();
-			auto&& t = e.addComponent<scene::components::TransformComponent>({
-				{ 600.0f, 300.0f, 0.0f },
-				{ 0.0f, 0.0f, 0.0f, 1.0f },
-				{ 200.0f, 200.f, 0.0f },
-			});
-			// t.scale *= 1'000;
-			e.addComponent<scene::components::MeshComponent>({ atlasMesh, atlasPipeline });
-			arch::Logger::debug("{:|v}\n", e.handle());
-		}
-
-		{
 			ecs::Entity e = testScene->newEntity();
-			auto&& t = testScene->domain().addComponent<scene::components::TransformComponent>(
+			testScene->domain().addComponent<scene::components::TransformComponent>(
 				e,
 				{
-					{ 320.0f, 200.0f, 0.0f },
+					{ 0.0f, 0.0f, 0.0f },
 					{ 0.0f, 0.0f, 0.0f, 1.0f },
 					{ 1.0f, .5f, 0.0f },
 			}
 			);
-			t.scale *= 50;
 
 			testScene->domain().addComponent<scene::components::MeshComponent>(e, { mesh, pipeline });
-			// testScene->domain().addComponent<VelocityComponent>(e, float3{ 0.01f, .01f, 0.0f });
+			testScene->domain().addComponent<VelocityComponent>(e, float3{ 0.01f, .01f, 0.0f });
 		}
 
 		for (int i = 0; i < 5; i++) {
 			ecs::Entity e = testScene->newEntity();
-			auto&& t = testScene->domain().addComponent<scene::components::TransformComponent>(
+			testScene->domain().addComponent<scene::components::TransformComponent>(
 				e,
 				{
-					{ 320.f, 200.f, 0.0f },
+					{ -1 + 0.5f * i, 0.25f * i, 0.0f },
 					{ 0.0f, 0.0f, 0.0f, 1.0f },
-					{ 1, 1, 0 }
-			}
+					float3(1)
+			  }
 			);
-			t.scale *= 50;
 			testScene->domain().addComponent<scene::components::MeshComponent>(e, { mesh, pipeline2 });
-			// testScene->domain().addComponent<VelocityComponent>(e, float3{ .0f, .0f, 0.0f });
+			testScene->domain().addComponent<VelocityComponent>(e, float3{ 0.0f, -.01f, 0.0f });
 		}
 
 		scene::SceneManager::get()->changeScene(testScene);
 	}
 
 	void update() override {
-		static float frame = 0;
-		frame += 0.005;
-		auto view = scene::SceneManager::get()->currentScene()->domain().view<scene::components::TransformComponent>();
+		auto view = scene::SceneManager::get()
+						->currentScene()
+						->domain()
+						.view<scene::components::TransformComponent, VelocityComponent>();
 
-		for (auto&& [entity, transform] : view.all()) {
-			/*if ((transform.position.y < 0 && velocity.velocity.y < 0) ||
-				(transform.position.y > 1 * 50 && velocity.velocity.y > 0)) {
+		for (auto [entity, transform, velocity] : view.all()) {
+			if ((transform.position.y < -.5f && velocity.velocity.y < 0) ||
+				(transform.position.y > .5f && velocity.velocity.y > 0)) {
 				velocity.velocity.y *= -1;
 			}
-			if ((transform.position.x < 0 && velocity.velocity.x < 0) ||
-				(transform.position.x > 1 * 50 && velocity.velocity.x > 0)) {
+			if ((transform.position.x < -.5f && velocity.velocity.x < 0) ||
+				(transform.position.x > .5f && velocity.velocity.x > 0)) {
 				velocity.velocity.x *= -1;
-			}*/
+			}
 
-			/*transform.scale.x = */ // transform.scale.y = std::cos(frame) * 1'000;
+			transform.position.x += velocity.velocity.x;
+			transform.position.y += velocity.velocity.y;
 		}
 	}
 };
