1bf066964832b83462a12f7acaabf536dd8c1ff8
Twarug <patryk@twardosz.dev>
2025-03-01 11:53:57 +0100

    Handle suboptimal swapchain

--- src/platform/nvrhi/context/NvrhiVulkanContext.cpp
+++ src/platform/nvrhi/context/NvrhiVulkanContext.cpp
@@ -6,6 +6,7 @@
 #include "../../vulkan/VulkanUtils.h"
 #include "../NvrhiMessageCallback.h"
 #include "../NvrhiUtils.h"
+#include "../exception/NvrhiException.h"
 #include "Window.h"
 #include "nvrhi/vulkan.h"
 
@@ -130,6 +131,10 @@ void NvrhiVulkanContext::beginFrame() {
 		}
 	}
 
+	if (!(res == VK_SUCCESS || res == VK_SUBOPTIMAL_KHR)) {
+		throw exception::NvrhiException("Failed to acquire swapchain image!");
+	}
+
 	_currentAcquireSemaphore = (_currentAcquireSemaphore + 1) % _acquireSemaphores.size();
 
 	if (res == VK_SUCCESS) {
@@ -159,7 +164,11 @@ void NvrhiVulkanContext::present() {
 	const Queue& presentQueue = getQueue(QueueType::presentaion);
 
 	VkResult res = vkQueuePresentKHR(presentQueue.queue, &info);
-	if (!(res == VK_SUCCESS || res == VK_ERROR_OUT_OF_DATE_KHR)) {
+	if (res == VK_SUBOPTIMAL_KHR || res == VK_ERROR_OUT_OF_DATE_KHR) {
+		_supportDetails = vulkan::VulkanSwapchain::SupportDetails::getSupportDetails(getPhysicalDevice(), _surface);
+
+		onResize(_supportDetails.capabilities.currentExtent.width, _supportDetails.capabilities.currentExtent.height);
+	} else if (res != VK_SUCCESS) {
 		throw vulkan::exceptions::VulkanException("Failed to present swapchain");
 	}
 
