57a43f9b4a4ddf927180ca555bddad2cbee750ec
Twarug <patryk@twardosz.dev>
2025-03-01 08:58:11 +0100

    Fix shader binding missmatch

--- run/shaders/fragment_atlas.glsl
+++ run/shaders/fragment_atlas.glsl
@@ -1,6 +1,6 @@
 #version 450 core
 
-layout(binding = 0) uniform sampler2D tex;
+layout(binding = 0) uniform texture2D tex;
 layout(binding = 128) uniform sampler texSampler;
 
 layout(location = 0) in vec3 fragColor;
@@ -17,11 +17,11 @@ float median(float r, float g, float b) {
 }
 
 void main() {
-    vec3 msdf = texture(tex, texCoord).rgb;
+    vec3 msdf = texture(sampler2D(tex, texSampler), texCoord).rgb;
     float distance = median(msdf.r, msdf.g, msdf.b);
 
     float pxDist = screenPxRange() * (distance - 0.5f);
     float alpha = clamp(pxDist + 0.5f, 0.0f, 1.0f);
 
-    outColor = vec4(vec3(alpha), 0);
+    outColor = vec4(vec3(alpha), 0.1);
 }
