f4958cdc42be1099456524d574f97fea91b3e1b0
Twarug <patryk@twardosz.dev>
2025-03-01 14:25:01 +0100

    Add uniform buffer to example

--- archimedes_bin/examples/Examples.cpp
+++ archimedes_bin/examples/Examples.cpp
@@ -1,5 +1,5 @@
 // todo: remove this after sbi_image integration to engine
-//#define STB_IMAGE_IMPLEMENTATION
-//#include <stb_image.h>
+// #define STB_IMAGE_IMPLEMENTATION
+// #include <stb_image.h>
 
 #include "Examples.h"
