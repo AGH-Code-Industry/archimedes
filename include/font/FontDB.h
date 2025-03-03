aecf59ab90ac2109b7b24c38328ac508fe575589
Chris-plusplus <72704303+Chris-plusplus@users.noreply.github.com>
2025-03-03 14:01:27 +0100

    Update FontDB.h

--- include/font/FontDB.h
+++ include/font/FontDB.h
@@ -11,6 +11,10 @@
 #include <utils/StringViewComparator.h>
 #include <utils/StringViewHasher.h>
 
+namespace arch {
+class Engine;
+}
+
 namespace arch::font {
 
 class Font;
@@ -26,8 +30,9 @@ public:
 
 	static FontDB& get() noexcept;
 
-	bool exists(std::string_view familyName) const noexcept;
-	OptRef<Font> get(std::string_view familyName) noexcept;
+	bool exists(std::string_view name) const noexcept;
+	OptRef<Font> get(std::string_view name) noexcept;
+	OptRef<Font> operator[](std::string_view name) noexcept;
 
 	MapType::const_iterator begin() const noexcept;
 	MapType::const_iterator end() const noexcept;
@@ -35,12 +40,18 @@ public:
 private:
 
 	friend class Font;
+	friend class ::arch::Engine;
 
 	FontDB() noexcept;
 
 	static std::unique_ptr<FontDB> _singleton;
 
 	void _findAndAddFonts() noexcept;
+#if ARCHIMEDES_WINDOWS
+	void _findAndAddFontsWindows() noexcept;
+#elif ARCHIMEDES_LINUX
+	void _findAndAddFontsLinux() noexcept;
+#endif
 
 	MapType _fonts;
 	char* _pimpl;
