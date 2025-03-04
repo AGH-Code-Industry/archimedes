42c9df167d23912f74943c9b92d15b09c9d53ce3
Chris-plusplus <72704303+Chris-plusplus@users.noreply.github.com>
2025-03-04 12:43:33 +0100

    Update Font.h

--- include/Font.h
+++ include/Font.h
@@ -3,6 +3,7 @@
 #include "font/Face.h"
 #include "font/Font.h"
 #include "font/FontDB.h"
+#include "font/FontNotFoundException.h"
 #include "font/FontStyle.h"
 #include "font/GlyphData.h"
 #include "font/MSDFAtlasGen.h"
