8ab8da622e40067636300eb5736468a8f307aba9
Chris-plusplus <72704303+Chris-plusplus@users.noreply.github.com>
2025-03-04 12:48:03 +0100

    Working TextRenderTestApp

--- archimedes_bin/examples/text_render/TextRenderTestApp.h
+++ archimedes_bin/examples/text_render/TextRenderTestApp.h
@@ -1,20 +1,35 @@
 #pragma once
 
+#include <codecvt>
+#include <locale>
+
 #include <Ecs.h>
 #include <Engine.h>
+#include <Font.h>
 #include <Scene.h>
-#define STB_IMAGE_IMPLEMENTATION
-#include <stb_image.h>
+#include <Text.h>
 
 using namespace arch;
 
 class TextRenderTestApp: public Application {
+	struct LineFlag {
+		static constexpr bool flagComponent = true;
+	};
+
+	struct TextInfo {
+		float3 topLeft{};
+		float3 bottomRight{};
+	};
+
 	void init() override {
-		int windowWidth = 800;
 		int windowWidth = 1'200;
 		int windowHeight = 600;
 
+		int baseline = windowHeight / 2;
+
 		Ref<Scene> testScene = createRef<Scene>();
+
+		scene::SceneManager::get()->changeScene(testScene);
 
 		// 2D square
 		struct Vertex {
@@ -22,48 +37,18 @@ class TextRenderTestApp: public Application {
 			float2 tex_coords;
 		};
 
-		std::vector<Vertex> vertices{
-			{  { 0.f, 0.f, 0.1f }, { 0.f, 1.f } },
-			{ { -1.f, 0.f, 0.1f }, { 1.f, 1.f } },
-			{  { 0.f, 1.f, 0.1f }, { 0.f, 0.f } },
-			{ { -1.f, 1.f, 0.1f }, { 1.f, 0.f } },
-		};
-		std::vector<u32> indices{ 0, 1, 2 };
-		std::vector<u32> atlasIndices{ 0, 1, 2, 2, 1, 3 };
+		std::vector<u32> indices{ 0, 1, 2, 2, 1, 3 };
 
-		// Ref<Shader> vShader = Shader::load("shaders/vertex_shader.sprv");
-		// Ref<Shader> fShader = Shader::load("shaders/fragment_shader.sprv");
-		// Ref<Material> material = Material::create(vShader, fShader);
-		// material->setTexture("_mainTxt", TextureLoader::read_file("textures/.jpg"));
-		// material->SetFloat("_mixValue", 0.2f);
-		// material->SetFloat3("_pos", glm::vec3(0.5f, 0.5f, 0.5f));
-		// material->SetColor("_color", glm::vec3(1.0f, 0.0f, 0.0f));
-
-		Color pixels[] = {
-			Color{ 1, .5, 1, 1 }
+		std::vector<Vertex> lineVertices{
+			{  { 0.f, 0.f, 0.1f }, {} },
+			{ { -1.f, 0.f, 0.1f }, {} },
+			{  { 0.f, 1.f, 0.1f }, {} },
+			{ { -1.f, 1.f, 0.1f }, {} },
 		};
 
 		Ref<gfx::Renderer> renderer = gfx::Renderer::getCurrent();
 
-		Ref<gfx::texture::Texture> texture = renderer->getTextureManager()->createTexture2D(1, 1, pixels);
-
-		stbi_set_flip_vertically_on_load(true);
-		int atlasWidth, atlasHeight, nChannels;
-		unsigned char* data = stbi_load("atlas.png", &atlasWidth, &atlasHeight, &nChannels, STBI_rgb_alpha);
-		assert(data);
-
-		auto atlasPixels = std::vector<Color>(atlasWidth * atlasHeight);
-		for (u32 i = 0; i != atlasPixels.size(); ++i) {
-			atlasPixels[i].r = (float)data[4 * i + 0] / 255.f;
-			atlasPixels[i].g = (float)data[4 * i + 1] / 255.f;
-			atlasPixels[i].b = (float)data[4 * i + 2] / 255.f;
-			atlasPixels[i].a = (float)data[4 * i + 3] / 255.f;
-		}
-		stbi_image_free(data);
-
-		auto atlasTexture = renderer->getTextureManager()->createTexture2D(atlasWidth, atlasHeight, atlasPixels.data());
-
-		Ref<asset::mesh::Mesh> atlasMesh = asset::mesh::Mesh::create<Vertex>(vertices, atlasIndices);
+		Ref<asset::mesh::Mesh> lineMesh = asset::mesh::Mesh::create<Vertex>(lineVertices, indices);
 
 		struct UniformBuffer {
 			Mat4x4 projection;
@@ -73,39 +58,47 @@ class TextRenderTestApp: public Application {
 		auto uniformBuffer =
 			renderer->getBufferManager()->createBuffer(gfx::BufferType::uniform, &ubo, sizeof(UniformBuffer));
 
-		auto pipeline = renderer->getPipelineManager()->create({
-			.vertexShaderPath = "shaders/vertex_default.glsl",
-			.fragmentShaderPath = "shaders/fragment_default.glsl",
-			.textures = { texture },
-			.buffers = { uniformBuffer },
-		});
-		auto pipeline2 = renderer->getPipelineManager()->create({
+		auto linePipeline = renderer->getPipelineManager()->create({
 			.vertexShaderPath = "shaders/vertex_default.glsl",
 			.fragmentShaderPath = "shaders/fragment_default2.glsl",
 			.textures = {},
 			.buffers = { uniformBuffer },
 		});
-		auto atlasPipeline = renderer->getPipelineManager()->create({
-			.vertexShaderPath = "shaders/vertex_default.glsl",
-			.fragmentShaderPath = "shaders/fragment_atlas.glsl",
-			.textures = { atlasTexture },
-			.buffers = { uniformBuffer },
-		});
-
-		Ref<asset::mesh::Mesh> mesh = asset::mesh::Mesh::create<Vertex>(vertices, indices);
 
 		{
-			auto e = testScene->newEntity();
-			auto&& t = e.addComponent<scene::components::TransformComponent>({
-				{ 0, windowHeight, 0.0f },
-				{ 0.0f, 0.0f, 0.0f, 1.0f },
-				{ windowHeight, windowHeight, 0.0f },
-			});
-			// t.scale *= 1'000;
-			e.addComponent<scene::components::MeshComponent>({ atlasMesh, atlasPipeline });
-			arch::Logger::debug("{:|v}\n", e.handle());
+			auto line = testScene->newEntity();
+			auto&& t = line.addComponent<scene::components::TransformComponent>({
+				{ 0, baseline, 0 },
+				{ 0, 0, 0, 1 },
+				{ windowWidth, 1, 0 }
+			  });
+
+			line.addComponent<scene::components::MeshComponent>({ lineMesh, linePipeline });
+			line.addComponent<LineFlag>();
 		}
 
-		scene::SceneManager::get()->changeScene(testScene);
+		auto&& tc = testScene->newEntity()
+						.addComponent<text::TextComponent>()
+						.setFont("Arial", "Regular")
+						.setFontSize(70)
+						.setBaseline({ 0, windowHeight / 2 })
+						.setText<char32_t>(U"Zażółć gęślą jaźń <3 AV");
+		tc.updateText();
+		auto width = (tc.bottomRight() - tc.topLeft()).x;
+		// vv- how to center <div> -vv
+		tc.setBaseline({ (1200.f - width) / 2, windowHeight / 2 });
+		tc.updateText();
+	}
+
+	void update() {
+		std::this_thread::sleep_for(std::chrono::milliseconds(16));
+
+		for (auto&& [ent, tr, tc] : scene::SceneManager::get()
+										->currentScene()
+										->domain()
+										.view<scene::components::TransformComponent, text::TextComponent>()
+										.all()) {
+			tc.rotateDeg(1.f);
+		}
 	}
 };
