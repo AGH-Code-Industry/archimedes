bbd26e0eadac586e5206a57cadd93d9214018404
Chris-plusplus <72704303+Chris-plusplus@users.noreply.github.com>
2025-03-27 09:07:32 +0100

    bigger window

--- archimedes_bin/main.cpp
+++ archimedes_bin/main.cpp
@@ -7,8 +7,8 @@ int main() {
 
 	arch::Ref<arch::Application> myApp = arch::createRef<WulkanApp>();
 
-	arch::EngineConfig config{ .windowWidth = 1'200,
-							   .windowHeight = 600,
+	arch::EngineConfig config{ .windowWidth = 1'600,
+							   .windowHeight = 900,
 							   .windowTitle = "Archimedes Test",
 							   .backgroundColor = arch::Color(.0f, .0f, .0f, 1.f),
 							   .renderingApi = arch::gfx::RenderingAPI::Nvrhi_VK };
