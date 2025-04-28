ff5873b2f6c5085b3363c5df13009ec19d126f82
Chris-plusplus <72704303+Chris-plusplus@users.noreply.github.com>
2025-04-28 14:50:38 +0200

    Moved one-liners & frequently used methods from SparseSet.cpp to .hpp

--- include/ecs/SparseSet.h
+++ include/ecs/SparseSet.h
@@ -4,6 +4,8 @@
 #include <memory>
 #include <vector>
 
+#include "ViewFwd.h"
+//
 #include "EntityTraits.h"
 
 namespace arch::ecs::_details {
@@ -26,26 +28,31 @@ public:
 	/// @param entity - entity to find
 	/// @return Index to dense if found
 	/// @return (size_t)-1 if not found
-	size_t find(const EntityT entity) const noexcept;
+	inline size_t find(const EntityT entity) const noexcept;
 	/// @brief Find entity with given id
 	/// @param id - id of entity to find
 	/// @return Index to dense if found
 	/// @return (size_t)-1 if not found
-	size_t find(const IdT id) const noexcept;
+	inline size_t find(const IdT id) const noexcept;
 	/// @brief Checks if given entity is contained
 	/// @param entity - entity to check
-	bool contains(const EntityT entity) const noexcept;
+	inline bool contains(const EntityT entity) const noexcept;
 	/// @brief Checks if entity with given id is contained
 	/// @param entity - entity to check
-	bool contains(const IdT id) const noexcept;
+	inline bool containsID(const IdT id) const noexcept;
 	/// @brief Gets version of entity with equal id as given or null if not found
 	/// @param entity - entity to check
-	VersionT version(const EntityT entity) const noexcept;
+	inline VersionT version(const EntityT entity) const noexcept;
 	/// @brief Gets version of entity with given id or null if not found
 	/// @param entity - entity to check
-	VersionT version(const IdT id) const noexcept;
+	inline VersionT version(const IdT id) const noexcept;
 
 protected:
+
+	template<class, class>
+	friend class View;
+	template<class, class>
+	friend class ViewIterator;
 
 	using SparseContainer = std::vector<std::unique_ptr<Entity[]>>;
 	using DenseContainer = std::vector<Entity>;
@@ -53,16 +60,20 @@ protected:
 	// possibly inits sparse page
 	EntityT* _sparseAssurePage(const size_t n) noexcept;
 	// returns reference to sparse, inits if not found
-	EntityT& _sparseAssure(const IdT id) noexcept;
+	inline EntityT& _sparseAssure(const IdT id) noexcept;
 	// returns reference to sparse, UB if not found
-	EntityT& _sparseGet(const IdT id) noexcept;
-	const EntityT& _sparseGet(const IdT id) const noexcept;
+	inline EntityT& _sparseGet(const IdT id) noexcept;
+	inline const EntityT& _sparseGet(const IdT id) const noexcept;
+	inline const EntityT& _sparseCGet(const IdT id) const noexcept;
 	// returns pointer to sparse, nullptr if not found
-	EntityT* _sparseTryGet(const IdT id) noexcept;
-	const EntityT* _sparseTryGet(const IdT id) const noexcept;
+	inline EntityT* _sparseTryGet(const IdT id) noexcept;
+	inline const EntityT* _sparseTryGet(const IdT id) const noexcept;
+	inline const EntityT* _sparseTryCGet(const IdT id) const noexcept;
 
 	SparseContainer _sparse;
 	DenseContainer _dense;
 };
 
 } // namespace arch::ecs::_details
+
+#include "SparseSet.hpp"
