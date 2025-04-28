facfca4fad33f9c908379fe960bba583ea69f78f
Chris-plusplus <72704303+Chris-plusplus@users.noreply.github.com>
2025-04-28 14:48:21 +0200

    Added FlagComponentPred

--- include/ecs/Flag.h
+++ include/ecs/Flag.h
@@ -6,4 +6,9 @@ namespace arch::ecs::_details { // NOLINT
 template<class T>
 concept FlagComponent = T::flag_component || T::flagComponent || T::FlagComponent;
 
+template<class T>
+struct FlagComponentPred {
+	static constexpr bool value = FlagComponent<T>;
+};
+
 } // namespace arch::ecs::_details
