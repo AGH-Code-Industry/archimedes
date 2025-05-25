16b60f4aba3795285a00c43153e3cbb975176fd6
Piotr Walczak <106607310+omni-drft@users.noreply.github.com>
2025-05-26 01:59:51 +0200

    switching to loading png directly. Loading still to be imported.

---
+++ include/asset_manager/assets/Texture.h
@@ -0,0 +1,14 @@
+#pragma once 
+
+namespace arch::assetManager::assets {
+
+class Texture {
+public:
+	// TODO: add ctor
+	Texture() {}
+
+private:
+	// TODO: add texture buffers
+};
+
+} // namespace arch::assetManager::assets
--- include/asset_manager/importers/TextureImporter.h
+++ include/asset_manager/importers/TextureImporter.h
@@ -3,10 +3,9 @@
 #include "IAssetImporter.h"
 
 #include <ktx.h>
+#include <vulkan/vulkan_core.h>
 #define STB_IMAGE_IMPLEMENTATION
 #include <stb_image.h>
-#define STB_IMAGE_RESIZE_IMPLEMENTATION
-#include <stb_image_resize.h>
 
 #include <vector>
 #include <cmath>
@@ -15,37 +14,10 @@
 
 namespace arch::assetManager {
 
-enum class MipMapGenerationMethod : uint8_t {
-	DEFAULT,
-	BOX_FILTER,
-	TRIANGLE_FILTER,
-	MITCHELL_NETRAVAL_FILTER,
-};
-
 class TextureImporter: public IAssetImporter {
 public:
 	bool SupportsFile(const std::filesystem::path& path) const override;
 	void Import(const std::filesystem::path& sourcePath, const std::filesystem::path& processedPath) const override;
-	void SetMipMapGenerationProperties(
-		MipMapGenerationMethod genMethod = MipMapGenerationMethod::DEFAULT,
-		uint32_t mipMapLevels = 0,
-		uint32_t minAllowedDimension = 1
-	);
-
-private:
-
-	struct TextureData {
-		std::vector<uint8_t> pixels;
-		int width;
-		int height;
-		int channels;
-	};
-
-private:
-
-	MipMapGenerationMethod _genMethod{ MipMapGenerationMethod::DEFAULT };
-	mutable uint32_t _mipMapLevels{ 0 };
-	uint32_t _minAllowedDimension{ 1 };
 };
 
 } // namespace arch::assetManager
