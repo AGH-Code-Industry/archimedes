2e8fec56532cc87311a4cb4fd4ab5ccdafc01d38
Piotr Walczak <106607310+omni-drft@users.noreply.github.com>
2025-05-25 02:13:58 +0200

    Adding mip map structure and starting its creation

--- include/asset_manager/importers/TextureImporter.h
+++ include/asset_manager/importers/TextureImporter.h
@@ -1,24 +1,49 @@
 #pragma once
 
 #include "IAssetImporter.h"
-
 #include <ktx.h>
-
 #define STB_IMAGE_IMPLEMENTATION
 #include <stb_image.h>
 #define STB_IMAGE_RESIZE_IMPLEMENTATION
-#include <stb_image_resize.h>
-
-
+#include <vector>
 
 #include <Logger.h>
+#include <stb_image_resize.h>
 
 namespace arch::assetManager {
+
+enum class MipMapGenerationMethod : uint8_t {
+	DEFAULT,
+	BOX_FILTER,
+	TRIANGLE_FILTER,
+	MITCHELL_NETRAVAL_FILTER,
+	LANCZOS3_FILTER,
+};
 
 class TextureImporter: public IAssetImporter {
 public:
 	bool SupportsFile(const std::filesystem::path& path) const override;
 	void Import(const std::filesystem::path& sourcePath, const std::filesystem::path& processedPath) const override;
+	void SetMipMapGenerationProperties(
+		MipMapGenerationMethod genMethod = MipMapGenerationMethod::DEFAULT,
+		uint32_t mipMapLevels = 0,
+		uint32_t minAllowedDimension = 1
+	);
+
+private:
+
+	struct TextureData {
+		std::vector<uint8_t> pixels;
+		int width;
+		int height;
+		int channels;
+	};
+
+private:
+
+	mutable MipMapGenerationMethod _genMethod{ MipMapGenerationMethod::DEFAULT };
+	mutable uint32_t _mipMapLevels = 0;
+	mutable uint32_t _minAllowedDimension = 1;
 };
 
 } // namespace arch::assetManager
--- src/asset_manager/importers/TextureImporter.cpp
+++ src/asset_manager/importers/TextureImporter.cpp
@@ -12,37 +12,57 @@ bool TextureImporter::SupportsFile(const std::filesystem::path& path) const {
 	}
 }
 
-void TextureImporter::Import(const std::filesystem::path& sourcePath, const std::filesystem::path& processedPath)
-	const {
+void TextureImporter::Import(const std::filesystem::path& sourcePath, const std::filesystem::path& processedPath) {
 	if (!std::filesystem::exists(sourcePath)) {
 		arch::Logger::error("File not found - '{}'", sourcePath.string());
 		return;
 	} else {
-		arch::Logger::trace("File succesfully located - '{}'", sourcePath.string());
+		arch::Logger::trace("File successfully located - '{}'", sourcePath.string());
 	}
 
-	// TODO: Read texture dimensions and channels
-	int imageWidth{}, imageHeight{}, channels{};
+	TextureData image{};
 
-	uint8_t* data{ stbi_load(sourcePath.string().c_str(), &imageWidth, &imageHeight, &channels, 0) };
+	uint8_t* data{ stbi_load(sourcePath.string().c_str(), &image.width, &image.height, &image.channels, 0) };
 	if (!data) {
 		arch::Logger::error("Error reading PNG ('{}'): {}", sourcePath.string(), stbi_failure_reason());
 		return;
 	}
 
-	if (channels < 4) {
+	if (image.channels < 4) {
 		arch::Logger::warn("Missing alpha channel ('{}')", sourcePath.string());
 		// TODO: Convert RGB to RGBA in case of missing alpha channel
 	}
 
-	std::vector<uint8_t> pixels{};
-	pixels.assign(
+	image.pixels.assign(
 		data,
-		data + static_cast<size_t>(imageWidth) * static_cast<size_t>(imageHeight) * static_cast<size_t>(channels)
+		data +
+			static_cast<size_t>(image.width) * static_cast<size_t>(image.height) * static_cast<size_t>(image.channels)
 	);
 
 	stbi_image_free(data);
-	arch::Logger::trace("Pixels read succesfully ('{}')", sourcePath.string());
+	arch::Logger::trace("Pixels read successfully ('{}')", sourcePath.string());
+
+	uint32_t maxMipMapLevels{ static_cast<uint32_t>(std::floor(std::log2(std::max(image.width, image.height)))) + 1 };
+
+	if (_mipMapLevels > maxMipMapLevels && _mipMapLevels != 0) {
+		arch::Logger::warn(
+			"Declared amount of mipmaps to generate bigger than possible ('{}'). Generating as many as possible.",
+			sourcePath.string()
+		);
+		_mipMapLevels = maxMipMapLevels;
+	}
+
+	std::vector<TextureData> mipMaps;
+}
+
+void TextureImporter::SetMipMapGenerationProperties(
+	MipMapGenerationMethod genMethod,
+	uint32_t mipMapLevels,
+	uint32_t minAllowedDimension
+) {
+	_genMethod = genMethod;
+	_mipMapLevels = mipMapLevels;
+	_minAllowedDimension = minAllowedDimension;
 }
 
 } // namespace arch::assetManager
