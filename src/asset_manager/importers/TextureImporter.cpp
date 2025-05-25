0b3d573125f296d2fd81c0243ba62444f3b8fe7d
Piotr Walczak <106607310+omni-drft@users.noreply.github.com>
2025-05-25 20:59:04 +0200

    Generating mipmaps.

--- src/asset_manager/importers/TextureImporter.cpp
+++ src/asset_manager/importers/TextureImporter.cpp
@@ -12,7 +12,8 @@ bool TextureImporter::SupportsFile(const std::filesystem::path& path) const {
 	}
 }
 
-void TextureImporter::Import(const std::filesystem::path& sourcePath, const std::filesystem::path& processedPath) {
+void TextureImporter::Import(const std::filesystem::path& sourcePath, const std::filesystem::path& processedPath)
+	const {
 	if (!std::filesystem::exists(sourcePath)) {
 		arch::Logger::error("File not found - '{}'", sourcePath.string());
 		return;
@@ -30,7 +31,13 @@ void TextureImporter::Import(const std::filesystem::path& sourcePath, const std:
 
 	if (image.channels < 4) {
 		arch::Logger::warn("Missing alpha channel ('{}')", sourcePath.string());
-		// TODO: Convert RGB to RGBA in case of missing alpha channel
+		stbi_image_free(data);
+		data = stbi_load(sourcePath.string().c_str(), &image.width, &image.height, &image.channels, STBI_rgb_alpha);
+		if (!data) {
+			arch::Logger::error("Error reloading PNG as RGBA ('{}'): {}", sourcePath.string(), stbi_failure_reason());
+			return;
+		}
+		image.channels = 4;
 	}
 
 	image.pixels.assign(
@@ -53,6 +60,85 @@ void TextureImporter::Import(const std::filesystem::path& sourcePath, const std:
 	}
 
 	std::vector<TextureData> mipMaps;
+	mipMaps.emplace_back(image);
+
+	TextureData currMipSrc{ image };
+
+	for (size_t level{ 1 }; level < _mipMapLevels; ++level) {
+		size_t nextWidth{ std::max(1, currMipSrc.width / 2) };
+		size_t nextHeight{ std::max(1, currMipSrc.height / 2) };
+
+		if (((nextWidth < _minAllowedDimension || nextHeight < _minAllowedDimension) && _minAllowedDimension > 1) ||
+			(currMipSrc.width == 1 && currMipSrc.height == 1)) {
+			break;
+		}
+
+		if (_minAllowedDimension > 1 && (nextWidth < _minAllowedDimension || nextHeight < _minAllowedDimension)) {
+			bool stop = false;
+			if (nextWidth < _minAllowedDimension && currMipSrc.width >= _minAllowedDimension) {
+				stop = true;
+			}
+			if (nextHeight < _minAllowedDimension && currMipSrc.height >= _minAllowedDimension) {
+				stop = true;
+			}
+			if (stop) {
+				break;
+			}
+		}
+
+		TextureData nextMipLevel;
+		nextMipLevel.width = nextWidth;
+		nextMipLevel.height = nextHeight;
+		nextMipLevel.channels = currMipSrc.channels;
+		nextMipLevel.pixels.resize(
+			static_cast<size_t>(nextMipLevel.width) * nextMipLevel.height * nextMipLevel.channels
+		);
+
+		int alphaChannelIndex = (nextMipLevel.channels == 4 || nextMipLevel.channels == 2) ?
+			(nextMipLevel.channels - 1) :
+			STBIR_ALPHA_CHANNEL_NONE;
+		if (nextMipLevel.channels == 2) {
+			alphaChannelIndex = 1;
+		}
+
+		// TODO: apply _genMethod
+		stbir_filter filterMethod;
+
+		int resizeResult{ stbir_resize_uint8_generic(
+			currMipSrc.pixels.data(),
+			currMipSrc.width,
+			currMipSrc.height,
+			0,
+			nextMipLevel.pixels.data(),
+			nextMipLevel.width,
+			nextMipLevel.height,
+			0,
+			nextMipLevel.channels,
+			alphaChannelIndex,
+			0,
+			STBIR_EDGE_CLAMP,
+			filterMethod,
+			STBIR_COLORSPACE_SRGB,
+			NULL
+		) };
+
+		if (resizeResult == 0) {
+			arch::Logger::error(
+				"Mipmap level generation failed ('{}'): {}",
+				sourcePath.string(),
+				stbi_failure_reason()
+			);
+		}
+
+		mipMaps.emplace_back(nextMipLevel);
+		currMipSrc = nextMipLevel;
+
+		if (currMipSrc.width == 1 && currMipSrc.height == 1) {
+			break;
+		}
+	}
+
+	// TODO: Handle generated mipmaps
 }
 
 void TextureImporter::SetMipMapGenerationProperties(
