5b60f3c49432f69399c2e0207e1c6d0a4d6fbdfc
Chris-plusplus <72704303+Chris-plusplus@users.noreply.github.com>
2025-06-09 12:47:01 +0200

    Added keyboard input

---
+++ include/input/Input.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "KeyState.h"
+#include "KeyStateWrapper.h"
+#include "keyboard/Key.h"
+#include "keyboard/State.h"
+
+namespace arch {
+
+namespace keyboard = input::keyboard;
+
+using input::KeyState;
+
+} // namespace arch
