9c08b54362bad887f9ecf8e759eaeeff90508378
Chris-plusplus <72704303+Chris-plusplus@users.noreply.github.com>
2025-06-21 23:36:42 +0200

    Update Input.h

--- include/input/Input.h
+++ include/input/Input.h
@@ -5,10 +5,14 @@
 #include "keyboard/Accessors.h"
 #include "keyboard/Key.h"
 #include "keyboard/State.h"
+#include "mouse/Accessors.h"
+#include "mouse/Button.h"
+#include "mouse/State.h"
 
 namespace arch {
 
 namespace keyboard = input::keyboard;
+namespace mouse = input::mouse;
 
 using input::KeyState;
 
