3a527c73dea5f81fc36b2022091107ea9fcad595
Chris-plusplus <72704303+Chris-plusplus@users.noreply.github.com>
2025-06-22 00:10:55 +0200

    added InputTestApp

--- archimedes_bin/examples/Examples.h
+++ archimedes_bin/examples/Examples.h
@@ -1,11 +1,13 @@
 #pragma once
 
+#include "input/InputTestApp.h"
 #include "nvrhi_renderer/NvrhiRendererTestApp.h"
-#include "text_render/TextRenderTestApp.h"
 #include "physics/PhysicsTestApp.h"
-#include "audio/SpatialAudioTestApp.h"
-#include "audio/SimpleSoundTestApp.h"
-#include "audio/RewindTestApp.h"
+#include "text_render/TextRenderTestApp.h"
+// manual order, doesnt compile if sorted
 #include "audio/ControlTestApp.h"
+#include "audio/RewindTestApp.h"
+#include "audio/SimpleSoundTestApp.h"
+#include "audio/SpatialAudioTestApp.h"
 
 // todo: some sort of container for all examples to be able to choose them at runtime
