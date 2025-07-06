bb302c0d0d8a79d21b853c11c92aaaadf9cdb6c4
Piotr Walczak <106607310+omni-drft@users.noreply.github.com>
2025-07-07 01:13:57 +0200

    adding import testing

--- include/asset_manager/assets/Mesh.h
+++ include/asset_manager/assets/Mesh.h
@@ -10,13 +10,11 @@
 
 namespace arch::assetManager::assets {
 
-class Mesh {
-public:
+struct Mesh {
 	Mesh(std::vector<float> vertices, std::vector<uint32_t> indices):
 		_vertexBuffer(std::move(vertices)),
 		_indexBuffer(std::move(indices)) {}
 
-private:
 	std::vector<float> _vertexBuffer;
 	std::vector<uint32_t> _indexBuffer;
 };
