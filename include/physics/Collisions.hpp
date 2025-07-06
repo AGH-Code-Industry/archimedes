20322bfd56504c9e4435f61e544fdbf214f710ba
MagrosThornrime <magros.thornrime@gmail.com>
2025-07-07 01:53:09 +0200

    Fix another typo in collisions' template

--- include/physics/Collisions.hpp
+++ include/physics/Collisions.hpp
@@ -5,7 +5,7 @@ namespace arch::physics {
 
 	template<typename T1, typename T2>
 	concept CollidableReversed = requires(T2 a, T1 b) {
-		{ areColliding(a, b) } -> std::same_as<bool>;
+		{ checkCollision(a, b) } -> std::same_as<bool>;
 	};
 
 	template<typename T1, typename T2>
