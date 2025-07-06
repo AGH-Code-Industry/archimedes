6f72a23ee45f79c4807b845ab248aca25f44d0ae
Piotr Walczak <106607310+omni-drft@users.noreply.github.com>
2025-07-07 01:14:14 +0200

    Revert "adding import testing"
    This reverts commit bb302c0d0d8a79d21b853c11c92aaaadf9cdb6c4.

--- include/asset_manager/assets/Mesh.h
+++ include/asset_manager/assets/Mesh.h
@@ -10,11 +10,13 @@
 
 namespace arch::assetManager::assets {
 
-struct Mesh {
+class Mesh {
+public:
 	Mesh(std::vector<float> vertices, std::vector<uint32_t> indices):
 		_vertexBuffer(std::move(vertices)),
 		_indexBuffer(std::move(indices)) {}
 
+private:
 	std::vector<float> _vertexBuffer;
 	std::vector<uint32_t> _indexBuffer;
 };
