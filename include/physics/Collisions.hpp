cf3e8b1c64e9c8858924df4ea9d56edfe11fb4e3
MagrosThornrime <magros.thornrime@gmail.com>
2025-07-07 01:52:24 +0200

    Fix typo in collisions' template

--- include/physics/Collisions.hpp
+++ include/physics/Collisions.hpp
@@ -10,8 +10,8 @@ namespace arch::physics {
 
 	template<typename T1, typename T2>
 	requires CollidableReversed<T1, T2>
-	bool areColliding(T1 a, T2 b) {
-		return areColliding(b, a);
+	bool checkCollision(T1 a, T2 b) {
+		return checkCollision(b, a);
 	}
 
 	bool checkCollision(const AABB& lhs, const AABB& rhs);
