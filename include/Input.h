ac93a535a6fab9ab389494ff4d010ffa2a2f7012
Chris-plusplus <72704303+Chris-plusplus@users.noreply.github.com>
2025-07-08 23:04:44 +0200

    exposed important input classes in main namespace

--- include/Input.h
+++ include/Input.h
@@ -1,3 +1,18 @@
 #pragma once
 
-#include <input/Input.h>
+// #include "input/Input.h"
+#include "input/Key.h"
+
+// #include "input/Keyboard.h"
+// #include "input/Mouse.h"
+
+namespace arch {
+
+using input::Key;
+using input::Keyboard;
+using input::KeyCode;
+using input::KeyState;
+using input::Mouse;
+using input::MouseButtonCode;
+
+} // namespace arch
