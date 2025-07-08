ad2ba9de1648000c934a5bf4787cdbf5001cfd6d
Chris-plusplus <72704303+Chris-plusplus@users.noreply.github.com>
2025-07-08 23:10:08 +0200

    Added KeyState

--- include/input/KeyState.h
+++ include/input/KeyState.h
@@ -1,45 +1,42 @@
 #pragma once
 
-#include <chrono>
-
 #include <Mmath.h>
 
 namespace arch::input {
 
 /// @brief Enum with all key/button states
-struct KeyState {
-	enum Value : u32 {
-		up = 0,
-		released,
-		down,
-		pressed,
-		repeat,
+enum class KeyState : u32 {
+	up = 0,
+	released,
+	down,
+	pressed,
+	repeat,
 
-		shift,
-		ctrl,
-		control = ctrl,
-		alt,
-		super,
-		win = super,
-		windows = win,
-		command = super,
-		meta = super,
-		capsLock,
-		numLock,
-	};
+	_modBegin,
+	shift = _modBegin,
+	ctrl,
+	control = ctrl,
+	alt,
+	super,
+	win = super,
+	windows = win,
+	command = super,
+	meta = super,
+	capsLock,
+	numLock,
 };
 
 /// @brief Helper operator returning bitmask of given KeyState
 /// @param value - KeyState to get bitmask of
-consteval u32 operator*(KeyState::Value value);
+consteval u32 operator*(KeyState value);
 /// @brief Helper operator combining bitmasks of given KeyStates
 /// @param lhs - first KeyState
 /// @param rhs - second KeyState
-consteval u32 operator+(KeyState::Value lhs, KeyState::Value rhs);
+consteval u32 operator+(KeyState lhs, KeyState rhs);
 /// @brief Helper operator combining bitmask with given KeyState
 /// @param lhs - bitmask
 /// @param rhs - second KeyState
-consteval u32 operator+(u32 lhs, KeyState::Value rhs);
+consteval u32 operator+(u32 lhs, KeyState rhs);
 
 } // namespace arch::input
 
--- include/input/KeyState.hpp
+++ include/input/KeyState.hpp
@@ -2,15 +2,15 @@
 
 namespace arch::input {
 
-consteval u32 operator*(KeyState::Value value) {
-	return 1u << value;
+consteval u32 operator*(KeyState value) {
+	return 1u << (u32)value;
 }
 
-consteval u32 operator+(KeyState::Value lhs, KeyState::Value rhs) {
+consteval u32 operator+(KeyState lhs, KeyState rhs) {
 	return *lhs | *rhs;
 }
 
-consteval u32 operator+(u32 lhs, KeyState::Value rhs) {
+consteval u32 operator+(u32 lhs, KeyState rhs) {
 	return lhs | *rhs;
 }
 
