b76a7493c4c97ea89707976a7179ed57dc47d6d5
Chris-plusplus <72704303+Chris-plusplus@users.noreply.github.com>
2025-07-08 23:02:47 +0200

    removed previous version

--- include/input/keyboard/State.hpp
+++
@@ -1,9 +0,0 @@
-#include "State.h"
-
-namespace arch::input::keyboard::_details {
-
-KeyStateWrapper State::operator[](const u32 keyCode) noexcept {
-	return KeyStateWrapper{ .value = _state[keyCode], .time = _time[keyCode] };
-}
-
-} // namespace arch::input::keyboard::_details
