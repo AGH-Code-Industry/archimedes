a28824c7d41d2dce231c2e0c5952b80d8870aa67
MagrosThornrime <magros.thornrime@gmail.com>
2025-07-18 21:18:29 +0200

    Fix tests for physics system

--- tests/physics/System.cpp
+++ tests/physics/System.cpp
@@ -1,36 +1,48 @@
-#include <../../include/physics/RigidBodyComponent.h>
+#include <physics/RigidBodyComponent.h>
 #include <Ecs.h>
 #include <gtest/gtest.h>
 #include <physics/System.h>
+#include <Scene.h>
 
 namespace physics {
 namespace ecs = arch::ecs;
 namespace phy = arch::physics;
 namespace math = arch::math;
+namespace scene = arch::scene;
 
 TEST(Physics, TestUpdate) {
 	ecs::Domain domain;
 	phy::System system(domain);
 
-	auto e0 = domain.newEntity();
+	constexpr auto startV = math::float3(1.f, 0.f, 0.f);
 
 	// Create movable entity
-	phy::RigidBodyComponent& entity = domain.addComponent<phy::RigidBodyComponent>(e0);
-	entity.center = { .mass = 1.f, .position = math::float2(0.f, 0.f) };
-	constexpr auto startV = math::float2(1.f, 0.f);
-	entity.velocity = startV;
-
-	// Add force to it
-	entity.force = math::float2(1.f, 1.f);
+	auto e0 = domain.newEntity();
+	auto& rigidBody = domain.addComponent<phy::RigidBodyComponent>(
+		e0,
+		{
+			1.f,
+			{1.f, 1.f, 0.f},
+			startV
+		}
+	);
+	auto& transform = domain.addComponent<scene::components::TransformComponent>(
+		e0,
+		{
+			{0.f, 0.f, 0.f},
+			{0.f, 0.f, 0.f, 1.f},
+			math::float3(1)
+		}
+	);
 
 	// Update physics system
 	const math::f32 t = system.update();
 
 	// Expect entity to move accordingly to the old velocity
-	EXPECT_EQ(entity.center.position, t * startV);
+	EXPECT_EQ(transform.position, t * startV);
 
 	// Expect that velocity updated accordingly to the force
-	EXPECT_EQ(entity.velocity, startV + t * entity.force);
+	EXPECT_EQ(rigidBody.velocity, startV + t * rigidBody.force);
 }
 
 TEST(Physics, CheckCollisions) {
