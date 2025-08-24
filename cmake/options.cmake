0ca53c471ba99e12c3b45664577ce4c0c80a3cd2
Chris-plusplus <72704303+Chris-plusplus@users.noreply.github.com>
2025-08-24 18:15:24 +0200

    added options

---
+++ cmake/options.cmake
@@ -0,0 +1,11 @@
+include_guard()
+
+option(ARCHIMEDES_SHARED "Build archimedes as shared library (currently unsupported)" OFF)
+if(ARCHIMEDES_SHARED)
+	message(FATAL_ERROR "archimeds does not currently support building as shared library")
+endif()
+
+option(ARCHIMEDES_BUILD_TESTS "Build tests" ON)
+option(ARCHIMEDES_BUILD_EXAMPLES "Build examples" ON)
+
+option(ARCHIMEDES_INSTALL "Install archimedes" ON)
