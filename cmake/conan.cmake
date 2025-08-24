981599b488957d246b3746742ac0156affa2c9dd
Chris-plusplus <72704303+Chris-plusplus@users.noreply.github.com>
2025-08-25 00:13:40 +0200

    Update conan.cmake

--- cmake/conan.cmake
+++ cmake/conan.cmake
@@ -73,12 +73,6 @@ if(ARCHIMEDES_CONAN_INSTALL)
 		set(ARCHIMEDES_CONAN_COMPILER_RUNTIME_TYPE "-s:b compiler.runtime_type=${CMAKE_BUILD_TYPE} -s:h compiler.runtime_type=${CMAKE_BUILD_TYPE}")
 	endif()
 
-	if(${CMAKE_SYSTEM_NAME} STREQUAL "Linux")
-		if(NOT ${ARCHIMEDES_LINUX})
-			message(FATAL_ERROR "${ARCHIMEDES_LINUX} != 1")
-		endif()
-	endif()
-
 	# Set package manager settings for Linux
 	if(${ARCHIMEDES_LINUX})
 		set(ARCHIMEDES_CONAN_TOOLS_SYSTEM_PACKAGE_MANAGER_MODE "-c tools.system.package_manager:mode=install")
@@ -90,7 +84,7 @@ if(ARCHIMEDES_CONAN_INSTALL)
 
 	# run the install command
 	execute_process(
-		COMMAND conan install ./conanfile_dev.py -s:b build_type=${ARCHIMEDES_BUILD_TYPE} -s:b compiler.cppstd=${CMAKE_CXX_STANDARD} -s:h build_type=${ARCHIMEDES_BUILD_TYPE} -s:h compiler.cppstd=${CMAKE_CXX_STANDARD} ${ARCHIMEDES_CONAN_INSTALL_RUNTIME_TYPE} ${ARCHIMEDES_CONAN_SYSTEM_PACKAGE_MANAGER_SUDO} ${ARCHIMEDES_CONAN_SYSTEM_PACKAGE_MANAGER_MODE} -of=conan_files/${ARCHIMEDES_BUILD_TYPE} --build=missing -pr default
+		COMMAND conan install ./conanfile_dev.py -s:b build_type=${ARCHIMEDES_BUILD_TYPE} -s:b compiler.cppstd=${CMAKE_CXX_STANDARD} -s:h build_type=${ARCHIMEDES_BUILD_TYPE} -s:h compiler.cppstd=${CMAKE_CXX_STANDARD} ${ARCHIMEDES_CONAN_INSTALL_RUNTIME_TYPE} ${ARCHIMEDES_CONAN_TOOLS_SYSTEM_PACKAGE_MANAGER_SUDO} ${ARCHIMEDES_CONAN_TOOLS_SYSTEM_PACKAGE_MANAGER_MODE} -of=conan_files/${ARCHIMEDES_BUILD_TYPE} --build=missing -pr default
 		WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
 		OUTPUT_VARIABLE ARCHIEMDES_CONAN_INSTALL_OUTPUT
 		RESULT_VARIABLE ARCHIEMDES_CONAN_INSTALL_RESULT
