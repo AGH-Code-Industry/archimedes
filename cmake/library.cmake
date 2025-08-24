944a97cc6be2baa06fba9e94c4cd53d390a3396d
Chris-plusplus <72704303+Chris-plusplus@users.noreply.github.com>
2025-08-24 18:20:41 +0200

    removed manual installation of msdf-atlas-gen

--- cmake/library.cmake
+++ cmake/library.cmake
@@ -27,16 +27,3 @@ get_target_property(LINKED_LIBS ${PROJECT_NAME} LINK_LIBRARIES)
 message(STATUS "Libraries linked to ${PROJECT_NAME}: ${LINKED_LIBS}")
 
 #include("${PROJECT_SOURCE_DIR}/cmake/non_conan_deps.cmake")
-
-# install msdf-atlas-gen/1.3 Release
-if (NOT EXISTS "${PROJECT_SOURCE_DIR}/cmake/conan_files/msdf_atlas_gen_1_3_installed")
-    message(STATUS "msdf-atlas-gen not installed, installing...")
-    execute_process(
-        COMMAND conan install --requires=msdf-atlas-gen/1.3 --build=missing
-        WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
-    )
-
-    file(WRITE "${PROJECT_SOURCE_DIR}/cmake/conan_files/msdf_atlas_gen_1_3_installed" "")
-else()
-    message(STATUS "msdf-atlas-gen installed")
-endif()
