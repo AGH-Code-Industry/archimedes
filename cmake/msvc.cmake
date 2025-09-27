82a18f1353b61d025c0994e152234f1fc47e75b4
Chris-plusplus <72704303+Chris-plusplus@users.noreply.github.com>
2025-09-27 16:16:37 +0200

    enable inlining for RelWithDebInfo

--- cmake/msvc.cmake
+++ cmake/msvc.cmake
@@ -6,4 +6,7 @@ if(MSVC)
     add_compile_options("/permissive-")		# Conform to C++ standard
     add_compile_options("/Zc:preprocessor")	# Conform to C++ standard (preprocessor)
     add_compile_options("/Zc:__cplusplus")	# Set the __cplusplus macro
+	
+	# Enable more agressive inling in RelWithDebInfo, the /Ob1 setting is legacy yet still the default
+	string(REPLACE "/Ob1" "/Ob2" CMAKE_CXX_FLAGS_RELWITHDEBINFO "${CMAKE_CXX_FLAGS_RELWITHDEBINFO}")
 endif()
