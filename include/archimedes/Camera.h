3ed08b9d2ab0d8eb5cb9dcb60a99b8e4b6d84b03
Chris-plusplus <72704303+Chris-plusplus@users.noreply.github.com>
2026-05-20 20:04:36 +0200

    Added camera

---
+++ include/archimedes/Camera.h
@@ -0,0 +1,76 @@
+#pragma once
+
+#include <archimedes/Gfx.h>
+
+namespace arch {
+
+/// @brief Class of a camera
+class Camera {
+public:
+	/// @brief Camera bounds
+	struct Bounds {
+		/// @brief Bottom left bound
+		float2 bottomLeft;
+		/// @brief Top right bound
+		float2 topRight;
+	};
+
+	/// @brief Constructor
+	Camera() noexcept;
+
+	float2 pos() const noexcept;
+	void pos(float2 newPosition) noexcept;
+	void changePos(float2 delta) noexcept;
+
+	float2 extents() const noexcept;
+	void extents(float2 newExtents) noexcept;
+	void changeExtents(float2 delta) noexcept;
+
+	float2 rawExtents() const noexcept;
+	void rawExtents(float2 newExtents) noexcept;
+	void changeRawExtents(float2 delta) noexcept;
+
+	Bounds bounds() const noexcept;
+	void bounds(Bounds newBounds) noexcept;
+
+	Bounds rawBounds() const noexcept;
+	void rawBounds(Bounds newBounds) noexcept;
+
+	float2 zoom() const noexcept;
+	void zoom(float2 newZoom) noexcept;
+	void zoom(float newZoom) noexcept;
+
+	void zoomIn(float2 modifier) noexcept;
+	void zoomIn(float modifier) noexcept;
+	void zoomOut(float2 modifier) noexcept;
+	void zoomOut(float modifier) noexcept;
+
+	float rotation() const noexcept;
+	float rotationDeg() const noexcept;
+	void rotation(float newRotation) noexcept;
+	void rotationDeg(float newRotation) noexcept;
+	void rotate(float delta) noexcept;
+	void rotateDeg(float delta) noexcept;
+
+	float2 screenToWorldPos(float2 screenPos) const noexcept;
+	float2 normalToWorldPos(float2 normalizedDeviceCoordinates) const noexcept;
+
+	const Ref<gfx::Buffer>& buffer() const noexcept;
+
+private:
+	void _updateExtents() noexcept;
+	void _updateSinCos() noexcept;
+	void _updateBuffer() noexcept;
+
+	Ref<gfx::Buffer> _buffer;
+	Ref<Window> _window;
+	float2 _rawExtents;
+	float2 _extents;
+	float2 _position;
+	float2 _zoom;
+	float _rotation;
+	float _sinRot;
+	float _cosRot;
+};
+
+} // namespace arch
---
+++ src/Camera.cpp
@@ -0,0 +1,170 @@
+#include <archimedes/Camera.h>
+
+namespace arch {
+
+Camera::Camera() noexcept {
+	auto&& renderer = *gfx::Renderer::current();
+	_window = renderer.getWindow();
+
+	_buffer = renderer.getBufferManager()->createBuffer(gfx::BufferType::uniform);
+	_rawExtents = _window->size() / 2;
+	_position = {};
+	_zoom = { 1, 1 };
+	_rotation = 0;
+	_sinRot = 0;
+	_cosRot = 1;
+
+	_updateExtents();
+}
+
+void Camera::_updateBuffer() noexcept {
+	auto viewProj = glm::ortho(-_extents.x, _extents.x, -_extents.y, _extents.y) *
+		glm::rotate(glm::mat4(1.f), _rotation, float3(0, 0, 1)) *
+		glm::translate(glm::mat4(1.f), float3(-_position.x, -_position.y, 0.f));
+
+	_buffer->setData(&viewProj, sizeof(viewProj));
+}
+
+void Camera::_updateExtents() noexcept {
+	_extents = _rawExtents * _zoom;
+	_updateBuffer();
+}
+
+void Camera::_updateSinCos() noexcept {
+	_sinRot = std::sin(_rotation);
+	_cosRot = std::cos(_rotation);
+	_updateBuffer();
+}
+
+float2 Camera::pos() const noexcept {
+	return _position;
+}
+
+void Camera::pos(float2 newPosition) noexcept {
+	_position = newPosition;
+	_updateBuffer();
+}
+
+void Camera::changePos(float2 delta) noexcept {
+	pos(pos() + delta);
+}
+
+float2 Camera::extents() const noexcept {
+	return _extents;
+}
+
+void Camera::extents(float2 newExtents) noexcept {
+	_rawExtents = newExtents;
+	_zoom = { 1, 1 };
+	_updateExtents();
+}
+
+void Camera::changeExtents(float2 delta) noexcept {
+	extents(extents() + delta);
+}
+
+float2 Camera::rawExtents() const noexcept {
+	return _rawExtents;
+}
+
+void Camera::rawExtents(float2 newExtents) noexcept {
+	_rawExtents = newExtents;
+	_updateExtents();
+}
+
+void Camera::changeRawExtents(float2 delta) noexcept {
+	rawExtents(rawExtents() + delta);
+}
+
+Camera::Bounds Camera::bounds() const noexcept {
+	return Bounds{ .bottomLeft = _position - _extents, .topRight = _position + _extents };
+}
+
+void Camera::bounds(Bounds newBounds) noexcept {
+	_position = (newBounds.bottomLeft + newBounds.topRight) / 2.f;
+	_rawExtents = (newBounds.topRight - newBounds.bottomLeft) / 2.f;
+	_zoom = { 1, 1 };
+	_updateExtents();
+}
+
+Camera::Bounds Camera::rawBounds() const noexcept {
+	return Bounds{ .bottomLeft = _position - _rawExtents, .topRight = _position + _rawExtents };
+}
+
+void Camera::rawBounds(Bounds newBounds) noexcept {
+	_position = (newBounds.bottomLeft + newBounds.topRight) / 2.f;
+	_rawExtents = (newBounds.topRight - newBounds.bottomLeft) / 2.f;
+	_updateExtents();
+}
+
+float2 Camera::zoom() const noexcept {
+	return _zoom;
+}
+
+void Camera::zoom(float2 newZoom) noexcept {
+	_zoom = newZoom;
+	_updateExtents();
+}
+
+void Camera::zoom(float newZoom) noexcept {
+	zoom({ newZoom, newZoom });
+}
+
+void Camera::zoomIn(float2 modifier) noexcept {
+	zoom(zoom() / modifier);
+}
+
+void Camera::zoomIn(float modifier) noexcept {
+	zoom(zoom() / modifier);
+}
+
+void Camera::zoomOut(float2 modifier) noexcept {
+	zoom(zoom() * modifier);
+}
+
+void Camera::zoomOut(float modifier) noexcept {
+	zoom(zoom() * modifier);
+}
+
+float Camera::rotation() const noexcept {
+	return _rotation;
+}
+
+float Camera::rotationDeg() const noexcept {
+	return glm::degrees(_rotation);
+}
+
+void Camera::rotation(float newRotation) noexcept {
+	_rotation = newRotation;
+	_updateSinCos();
+}
+
+void Camera::rotationDeg(float newRotation) noexcept {
+	_rotation = glm::radians(newRotation);
+	_updateSinCos();
+}
+
+void Camera::rotate(float delta) noexcept {
+	rotation(rotation() + delta);
+}
+
+void Camera::rotateDeg(float delta) noexcept {
+	rotation(rotation() + glm::radians(delta));
+}
+
+float2 Camera::screenToWorldPos(float2 screenPos) const noexcept {
+	return normalToWorldPos(screenPos / (float2)_window->size() * 2.f - 1.f);
+}
+
+float2 Camera::normalToWorldPos(float2 normalizedDeviceCoordinates) const noexcept {
+	normalizedDeviceCoordinates *= _extents;
+	return float2{ _cosRot * normalizedDeviceCoordinates.x + _sinRot * normalizedDeviceCoordinates.y,
+				   -_sinRot * normalizedDeviceCoordinates.x + _cosRot * normalizedDeviceCoordinates.y } +
+		_position;
+}
+
+const Ref<gfx::Buffer>& Camera::buffer() const noexcept {
+	return _buffer;
+}
+
+} // namespace arch
