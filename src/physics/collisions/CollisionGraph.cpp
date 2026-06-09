74d48f6e9427a5b2f009d961e6a05a7efa853943
Róża Zając <magros.thornrime@gmail.com>
2026-06-09 14:42:45 +0200

    Start replacing unordered_map<Entity, Collision> with ComponentPool<Collision>

--- src/physics/collisions/CollisionGraph.cpp
+++ src/physics/collisions/CollisionGraph.cpp
@@ -2,47 +2,65 @@
 
 
 namespace arch::physics {
-std::unordered_map<ecs::Entity, Collision> CollisionGraph::getCollisions(ecs::Entity entity) const {
-    if(_graph.contains(entity)){
-        return _graph.at(entity);
-    }
-    return {};
+OptRef<const ecs::ComponentPool<Collision>> CollisionGraph::getCollisions(ecs::Entity entity) const {
+	return _graph.tryGet(entity);
 }
 
 std::vector<ecs::Entity> CollisionGraph::getCollidingEntities() const {
     std::vector<ecs::Entity> result;
-    for(auto& [entity, _] : _graph) {
-        result.push_back(entity);
+	for (auto node = _graph.begin(); node != _graph.end(); node++) {
+        result.push_back(node.entity());
     }
     return result;
 }
 
 void CollisionGraph::addCollision(ecs::Entity entity1, ecs::Entity entity2, const Collision& collision) {
-    if(!_graph.contains(entity1) && !_graph.contains(entity2)){
-        _graph[entity1][entity2] = collision;
+	auto currentCollisionsOpt = _graph.tryGet(entity1);
+    if (!currentCollisionsOpt) {
+		_graph.addComponent(entity1);
+		auto& currentCollisions = _graph.get(entity1);
+		currentCollisions.addComponent(entity2, collision);
+		return;
     }
+	currentCollisionsOpt.get().addComponent(entity2, collision);
 }
 
 void CollisionGraph::updateCollision(ecs::Entity entity1, ecs::Entity entity2, const Collision& collision) {
-    if(_graph.contains(entity1) && _graph[entity1].contains(entity2)){
-        _graph[entity1][entity2] = collision;
+	auto currentCollisionsOpt = _graph.tryGet(entity1);
+    if (!currentCollisionsOpt) {
+		return;
     }
+	auto currentCollision = currentCollisionsOpt.get().tryGet(entity2);
+    if (!currentCollision) {
+		return;
+    }
+	currentCollision.get() = collision;
 }
 
 void CollisionGraph::removeCollision(ecs::Entity entity1, ecs::Entity entity2) {
-    if(_graph.contains(entity1) && _graph[entity1].contains(entity2)){
-        _graph[entity1].erase(entity2);
+	auto currentCollisionsOpt = _graph.tryGet(entity1);
+	if (!currentCollisionsOpt) {
+		return;
+	}
+	auto& currentCollisions = currentCollisionsOpt.get();
+    if (currentCollisions.contains(entity2)) {
+		currentCollisions.removeComponent(entity2);
     }
-    if(_graph[entity1].size() == 0) {
-        _graph.erase(entity1);
+    if (currentCollisions.count() == 0) {
+		_graph.removeComponent(entity1);
     }
 }
 
 std::optional<Collision> CollisionGraph::getCollision(ecs::Entity entity1, ecs::Entity entity2) const {
-    if(_graph.contains(entity1) && _graph.at(entity1).contains(entity2)){
-        return _graph.at(entity1).at(entity2);
-    }
-    return std::nullopt;
+    auto currentCollisionsOpt = _graph.tryGet(entity1);
+	if (!currentCollisionsOpt) {
+		return std::nullopt;
+	}
+	auto collisionOpt = currentCollisionsOpt.get().tryGet(entity2);
+	if (!collisionOpt) {
+		return std::nullopt;
+	}
+	return collisionOpt.get();
 }
 
 } // namespace arch::physics
