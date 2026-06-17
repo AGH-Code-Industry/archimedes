bde01213051d458386af3aabb359e5b909bbaedf
Chris-plusplus <72704303+Chris-plusplus@users.noreply.github.com>
2026-06-17 19:11:38 +0200

    Add Excluding View

---
+++ include/archimedes/ecs/view/ExcludingView.h
@@ -0,0 +1,49 @@
+#pragma once
+
+#include "ViewFwd.h"
+//
+#include "../ComponentPool.h"
+#include "ExcludeT.h"
+#include "GeneralViewIterator.h"
+#include <archimedes/utils/CallableTraits.h>
+#include <archimedes/utils/TypeList.h>
+
+namespace arch::ecs {
+
+class Domain;
+
+template<class... Excludes>
+requires(sizeof...(Excludes) != 0)
+class View<TypeList<>, TypeList<Excludes...>>:
+	public std::ranges::view_interface<View<TypeList<>, TypeList<Excludes...>>> {
+public:
+
+	using Iterator = ViewIterator<TypeList<>, TypeList<Excludes...>>;
+
+	static constexpr auto includes = typelist<>;
+	static constexpr auto excludes = typelist<Excludes...>;
+
+	View() noexcept = default;
+	View(Domain& domain) noexcept;
+
+	Iterator begin() const noexcept;
+	Iterator end() const noexcept;
+
+	void forEach(auto&& fn);
+
+	bool contains(const Entity entity) const noexcept;
+
+	// private:
+
+	using Traits = _details::EntityTraits::Version;
+
+	template<class, class>
+	friend class ViewIterator;
+
+	Domain* _domain = nullptr;
+	std::array<const _details::CommonComponentPool*, excludes.size()> _cpoolsExcl{};
+};
+
+} // namespace arch::ecs
+
+#include "ExcludingView.hpp"
---
+++ include/archimedes/ecs/view/ExcludingView.hpp
@@ -0,0 +1,48 @@
+#include "ExcludingView.h"
+
+#define TEMPLATE_E              \
+	template<class... Excludes> \
+	requires(sizeof...(Excludes) != 0)
+
+#define VIEW_E View<TypeList<>, TypeList<Excludes...>>
+
+namespace arch::ecs {
+
+TEMPLATE_E
+VIEW_E::View(Domain& domain) noexcept {
+	_domain = &domain;
+	_cpoolsExcl = { domain._tryGetCPool<Excludes>()... };
+}
+
+TEMPLATE_E
+VIEW_E::Iterator VIEW_E::begin() const noexcept {
+	return Iterator(*this, false);
+}
+
+TEMPLATE_E
+VIEW_E::Iterator VIEW_E::end() const noexcept {
+	return Iterator(*this, true);
+}
+
+TEMPLATE_E
+void VIEW_E::forEach(auto&& fn) {
+	for (const auto entity : _domain->entities()) {
+		auto contains = [&](const auto cpool) {
+			return cpool && cpool->contains(entity);
+		};
+		if (Traits::hasNotNull(entity) && std::ranges::none_of(_cpoolsExcl, contains)) {
+			fn(entity);
+		}
+	}
+}
+
+TEMPLATE_E
+bool VIEW_E::contains(const Entity entity) const noexcept {
+	auto contains = [&](const auto cpool) {
+		return cpool && cpool->contains(entity);
+	};
+
+	return Traits::hasNotNull(entity) && std::ranges::none_of(_cpoolsExcl, contains);
+}
+
+} // namespace arch::ecs
