f85a8cd67b3e69faa06493e04161fe088b299862
Chris-plusplus <72704303+Chris-plusplus@users.noreply.github.com>
2026-06-17 19:11:45 +0200

    Add General View

---
+++ include/archimedes/ecs/view/GeneralView.h
@@ -0,0 +1,67 @@
+#pragma once
+
+#include "ViewFwd.h"
+//
+#include "../ComponentPool.h"
+#include "ExcludeT.h"
+#include "GeneralViewIterator.h"
+#include <archimedes/utils/CallableTraits.h>
+#include <archimedes/utils/TypeList.h>
+
+namespace arch::ecs {
+
+class Domain;
+
+template<class... Includes, class... Excludes>
+requires(sizeof...(Includes) != 0 || sizeof...(Excludes) != 0)
+class View<TypeList<Includes...>, TypeList<Excludes...>>:
+	public std::ranges::view_interface<View<TypeList<Includes...>, TypeList<Excludes...>>> {
+public:
+
+	using Iterator = ViewIterator<TypeList<Includes...>, TypeList<Excludes...>>;
+
+	static constexpr auto includes = typelist<Includes...>;
+	static constexpr auto excludes = typelist<Excludes...>;
+
+	View() noexcept = default;
+	View(Domain& domain) noexcept;
+
+	Iterator begin() const noexcept;
+	Iterator end() const noexcept;
+
+	void forEach(auto&& fn);
+
+	auto comps() noexcept;
+	auto withEntity() noexcept;
+
+	auto comps(const Entity entity) noexcept;
+	bool contains(const Entity entity) const noexcept;
+
+	u32 minCPool() const noexcept;
+
+	// private:
+
+	using Traits = _details::EntityTraits::Version;
+
+	static consteval auto _availableComponents();
+
+	template<bool WithEntity, class... Cs>
+	void _forEach(auto&& fn, TypeList<Cs...> wanted);
+	template<class... Cs>
+	auto _comps(TypeList<Cs...> wanted) noexcept;
+	template<class... Cs>
+	auto _withEntity(TypeList<Cs...> wanted) noexcept;
+	template<class... Cs>
+	auto _comps(const Entity entity, TypeList<Cs...> wanted) noexcept;
+
+	template<class, class>
+	friend class ViewIterator;
+
+	std::array<_details::CommonComponentPool*, includes.size()> _cpools{};
+	std::array<const _details::CommonComponentPool*, excludes.size()> _cpoolsExcl{};
+	u32 _minCpoolIdx = (u32)-1;
+};
+
+} // namespace arch::ecs
+
+#include "GeneralView.hpp"
---
+++ include/archimedes/ecs/view/GeneralView.hpp
@@ -0,0 +1,275 @@
+#include "../IsEntity.h"
+#include "GeneralView.h"
+#include <archimedes/utils/IsApplicable.h>
+
+#define TEMPLATE_IE                                \
+	template<class... Includes, class... Excludes> \
+	requires(sizeof...(Includes) != 0 || sizeof...(Excludes) != 0)
+
+#define VIEW_IE View<TypeList<Includes...>, TypeList<Excludes...>>
+
+namespace arch::ecs {
+
+TEMPLATE_IE
+VIEW_IE::View(Domain& domain) noexcept {
+	_cpools = { (_details::CommonComponentPool*)domain._tryGetCPool<Includes>()... };
+	_cpoolsExcl = { (const _details::CommonComponentPool*)domain._tryGetCPool<Excludes>()... };
+	_minCpoolIdx = [&] {
+		u32 result = 0;
+		for (auto i = 0u; i != _cpools.size(); ++i) {
+			if (!_cpools[i] || _cpools[i]->count() == 0) {
+				return (u32)-1;
+			}
+			if (_cpools[i]->count() < _cpools[result]->count()) {
+				result = i;
+			}
+		}
+		return result;
+	}();
+}
+
+TEMPLATE_IE
+VIEW_IE::Iterator VIEW_IE::begin() const noexcept {
+	return Iterator(*this, false);
+}
+
+TEMPLATE_IE
+VIEW_IE::Iterator VIEW_IE::end() const noexcept {
+	return Iterator(*this, true);
+}
+
+TEMPLATE_IE
+consteval auto VIEW_IE::_availableComponents() {
+	constexpr auto nonFlags = includes.eraseIf<[]<class T>(TypeList<T>) {
+		return _details::ComponentTraits<T>::flag;
+	}>();
+
+	constexpr auto nonConstAsConst = nonFlags.eraseIf<std::is_const>().transform<std::add_const>();
+	return nonFlags + nonConstAsConst;
+}
+
+TEMPLATE_IE
+void VIEW_IE::forEach(auto&& fn) {
+	if (_minCpoolIdx == (u32)-1) {
+		return;
+	}
+
+	using Traits = utils::CallableTraits<decltype(fn)>;
+	constexpr auto nonFlags = includes.eraseIf<[]<class T>(TypeList<T>) {
+		return _details::ComponentTraits<T>::flag;
+	}>();
+
+	if constexpr (Traits::isCallable) {
+		constexpr auto args = Traits::args;
+		constexpr bool entityFirst = args.front().apply<_details::IsEntity>();
+		constexpr auto wanted = args.popFront<entityFirst>().transform<std::remove_reference>();
+
+		constexpr auto available = _availableComponents();
+
+		static_assert(available.containsAll(wanted), "wanted components exceed available");
+
+		_forEach<entityFirst>(std::forward<decltype(fn)>(fn), wanted);
+	} else if constexpr (utils::
+							 isApplicableV<decltype(fn), decltype(nonFlags.transform<std::add_lvalue_reference>())>) {
+		_forEach<false>(std::forward<decltype(fn)>(fn), nonFlags);
+	} else if constexpr (utils::isApplicableV<
+							 decltype(fn),
+							 decltype(typelist<Entity> + nonFlags.transform<std::add_lvalue_reference>())>) {
+		_forEach<true>(std::forward<decltype(fn)>(fn), nonFlags);
+	} else {
+		static_assert(Traits::isCallable, "forEach(): fn cannot be applied");
+	}
+}
+
+TEMPLATE_IE
+template<bool WithEntity, class... Cs>
+void VIEW_IE::_forEach(auto&& fn, TypeList<Cs...> wanted) {
+	constexpr auto cpoolsCast = wanted.transform<[]<class T>(TypeList<T> c) {
+		if constexpr (c.apply<std::is_const>()) {
+			return typelist<const ComponentPool<std::remove_const_t<T>>*>;
+		} else {
+			return typelist<ComponentPool<T>*>;
+		}
+	}>();
+
+	const auto cpoolsBegin = _cpools.cbegin();
+	const auto cpoolsMin = cpoolsBegin + _minCpoolIdx;
+	const auto cpoolsMinNext = cpoolsMin + 1;
+	const auto cpoolsEnd = _cpools.cend();
+
+	for (const auto entity : (*cpoolsMin)->_dense) {
+		auto contains = [&](const auto cpool) {
+			return cpool && cpool->contains(entity);
+		};
+
+		bool validEntity = Traits::hasNotNull(entity) && std::all_of(cpoolsBegin, cpoolsMin, contains) &&
+			std::all_of(cpoolsMinNext, cpoolsEnd, contains);
+		if constexpr (excludes.size() != 0) {
+			validEntity = validEntity && std::ranges::none_of(_cpoolsExcl, contains);
+		}
+
+		if (validEntity) {
+			if constexpr (WithEntity) {
+				fn(entity,
+				   reinterpret_cast<getType<cpoolsCast.get<wanted.find(typelist<Cs>)>()>>(
+					   _cpools[includes.find(typelist<Cs>)]
+				   )
+					   ->get(entity)...);
+			} else {
+				fn(reinterpret_cast<getType<cpoolsCast.get<wanted.find(typelist<Cs>)>()>>(
+					   _cpools[includes.find(typelist<Cs>)]
+				)
+					   ->get(entity)...);
+			}
+		}
+	}
+}
+
+TEMPLATE_IE
+auto VIEW_IE::comps() noexcept {
+	constexpr auto nonFlags = includes.eraseIf<[]<class T>(TypeList<T>) {
+		return _details::ComponentTraits<T>::flag;
+	}>();
+	if constexpr (includes.size() == 1) {
+		static_assert(nonFlags.size() != 0, "Cannot call comps() on flag-only views");
+		constexpr auto cpool = nonFlags.transform<[]<class T>(TypeList<T> c) {
+			if constexpr (c.apply<std::is_const>()) {
+				return typelist<const ComponentPool<std::remove_const_t<T>>*>;
+			} else {
+				return typelist<ComponentPool<T>*>;
+			}
+		}>();
+
+		if (_cpools.front()) {
+			return std::views::zip(std::views::all(*reinterpret_cast<getType<cpool>>(_cpools.front())));
+		} else {
+			static std::remove_pointer_t<getType<cpool>> dummy;
+			return std::views::zip(std::views::all(dummy));
+		}
+	} else {
+		return _comps(nonFlags);
+	}
+}
+
+TEMPLATE_IE
+template<class... Cs>
+auto VIEW_IE::_comps(TypeList<Cs...> wanted) noexcept {
+	constexpr auto cpoolsCast = wanted.transform<[]<class T>(TypeList<T> c) {
+		if constexpr (c.apply<std::is_const>()) {
+			return typelist<const ComponentPool<std::remove_const_t<T>>*>;
+		} else {
+			return typelist<ComponentPool<T>*>;
+		}
+	}>();
+
+	return (*this) | std::views::all | std::views::transform([this](const Entity entity) {
+			   return std::tie(
+				   reinterpret_cast<getType<cpoolsCast.get<wanted.find(typelist<Cs>)>()>>(
+					   _cpools[includes.find(typelist<Cs>)]
+				   )
+					   ->get(entity)...
+			   );
+		   });
+}
+
+TEMPLATE_IE
+auto VIEW_IE::withEntity() noexcept {
+	constexpr auto nonFlags = includes.eraseIf<[]<class T>(TypeList<T>) {
+		return _details::ComponentTraits<T>::flag;
+	}>();
+	if constexpr (includes.size() == 1) {
+		static_assert(nonFlags.size() != 0, "Cannot call comps() on flag-only views");
+		constexpr auto cpool = nonFlags.transform<[]<class T>(TypeList<T> c) {
+			if constexpr (c.apply<std::is_const>()) {
+				return typelist<const ComponentPool<std::remove_const_t<T>>*>;
+			} else {
+				return typelist<ComponentPool<T>*>;
+			}
+		}>();
+
+		if (_cpools.front()) {
+			return std::views::zip(
+				std::views::all(*this),
+				std::views::all(*reinterpret_cast<getType<cpool>>(_cpools.front()))
+			);
+		} else {
+			static std::remove_pointer_t<getType<cpool>> dummy;
+			return std::views::zip(std::views::all(*this), std::views::all(dummy));
+		}
+	} else {
+		return _withEntity(nonFlags);
+	}
+}
+
+TEMPLATE_IE
+template<class... Cs>
+auto VIEW_IE::_withEntity(TypeList<Cs...> wanted) noexcept {
+	constexpr auto cpoolsCast = wanted.transform<[]<class T>(TypeList<T> c) {
+		if constexpr (c.apply<std::is_const>()) {
+			return typelist<const ComponentPool<std::remove_const_t<T>>*>;
+		} else {
+			return typelist<ComponentPool<T>*>;
+		}
+	}>();
+
+	return (*this) | std::views::all | std::views::transform([this](const Entity entity) {
+			   return std::tuple_cat(
+				   std::tuple(entity),
+				   std::tie(
+					   reinterpret_cast<getType<cpoolsCast.get<wanted.find(typelist<Cs>)>()>>(
+						   _cpools[includes.find(typelist<Cs>)]
+					   )
+						   ->get(entity)...
+				   )
+			   );
+		   });
+}
+
+TEMPLATE_IE
+auto VIEW_IE::comps(const Entity entity) noexcept {
+	constexpr auto nonFlags = includes.eraseIf<[]<class T>(TypeList<T>) {
+		return _details::ComponentTraits<T>::flag;
+	}>();
+	return _comps(entity, nonFlags);
+}
+
+TEMPLATE_IE
+template<class... Cs>
+auto VIEW_IE::_comps(const Entity entity, TypeList<Cs...> wanted) noexcept {
+	constexpr auto cpoolsCast = wanted.transform<[]<class T>(TypeList<T> c) {
+		if constexpr (c.apply<std::is_const>()) {
+			return typelist<const ComponentPool<std::remove_const_t<T>>*>;
+		} else {
+			return typelist<ComponentPool<T>*>;
+		}
+	}>();
+
+	return std::tie(
+		reinterpret_cast<getType<cpoolsCast.get<wanted.find(typelist<Cs>)>()>>(_cpools[includes.find(typelist<Cs>)])
+			->get(entity)...
+	);
+}
+
+TEMPLATE_IE
+bool VIEW_IE::contains(const Entity entity) const noexcept {
+	auto contains = [&](const auto cpool) {
+		return cpool && cpool->contains(entity);
+	};
+
+	bool contained = Traits::hasNotNull(entity) && std::ranges::all_of(_cpools, contains);
+	if constexpr (excludes.size() != 0) {
+		contained = contained && std::none_of(_cpoolsExcl, contains);
+	}
+
+	return contained;
+}
+
+TEMPLATE_IE
+u32 VIEW_IE::minCPool() const noexcept {
+	return _minCpoolIdx;
+}
+
+} // namespace arch::ecs
+
+#undef VIEW_IE
+#undef TEMPLATE_IE
