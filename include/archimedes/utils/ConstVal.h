c5bfd3e1358b53c7e0d08b8ab125b426dbd77f90
Chris-plusplus <72704303+Chris-plusplus@users.noreply.github.com>
2026-07-04 23:11:03 +0200

    Add ConstVal class, constexpr wrapper for basic types

---
+++ include/archimedes/utils/ConstVal.h
@@ -0,0 +1,74 @@
+#pragma once
+
+#include <concepts>
+
+namespace arch::utils {
+
+template<class T, T v>
+class ConstVal: public std::integral_constant<T, v> {
+public:
+	using InnerType = T;
+
+	template<class T>
+	requires(std::is_arithmetic_v<T>)
+	consteval operator T() const {
+		return T(v);
+	}
+
+	template<class T>
+	consteval operator bool() const {
+		return bool(v);
+	}
+};
+
+template<class T, T v>
+static constexpr const ConstVal<T, v> constval = {};
+
+#define MAKE_BINARY_OPERATOR(op)                                     \
+	template<class T1, class T2, T1 v1, T2 v2>                       \
+	consteval auto operator op(ConstVal<T1, v1>, ConstVal<T2, v2>) { \
+		return constval<decltype(v1 op v2), (v1 op v2)>;             \
+	}
+
+#define MAKE_UNARY_OPERATOR(op)                   \
+	template<class T, T v1>                       \
+	consteval auto operator op(ConstVal<T, v1>) { \
+		return constval<T, op v1>;                \
+	}
+
+MAKE_BINARY_OPERATOR(+)
+MAKE_BINARY_OPERATOR(-)
+MAKE_BINARY_OPERATOR(*)
+MAKE_BINARY_OPERATOR(/)
+MAKE_BINARY_OPERATOR(%)
+MAKE_BINARY_OPERATOR(^)
+MAKE_BINARY_OPERATOR(&)
+MAKE_BINARY_OPERATOR(|)
+MAKE_BINARY_OPERATOR(==)
+MAKE_BINARY_OPERATOR(!=)
+MAKE_BINARY_OPERATOR(<)
+MAKE_BINARY_OPERATOR(>)
+MAKE_BINARY_OPERATOR(<=>)
+MAKE_BINARY_OPERATOR(<=)
+MAKE_BINARY_OPERATOR(>=)
+MAKE_BINARY_OPERATOR(<<)
+MAKE_BINARY_OPERATOR(>>)
+MAKE_BINARY_OPERATOR(&&)
+MAKE_BINARY_OPERATOR(||)
+MAKE_UNARY_OPERATOR(~)
+MAKE_UNARY_OPERATOR(!)
+MAKE_UNARY_OPERATOR(+)
+MAKE_UNARY_OPERATOR(-)
+MAKE_UNARY_OPERATOR(++)
+MAKE_UNARY_OPERATOR(--)
+
+#undef MAKE_UNARY_OPERATOR
+#undef MAKE_BINARY_OPERATOR
+
+template<size_t v>
+using ConstSize = ConstVal<size_t, v>;
+
+template<size_t v>
+static constexpr const ConstSize<v> constsize = {};
+
+} // namespace arch::utils
