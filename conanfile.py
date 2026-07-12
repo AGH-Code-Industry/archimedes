23fa3473ca91c9a39c0950645ded814177b2b062
Chris-plusplus <72704303+Chris-plusplus@users.noreply.github.com>
2026-07-12 21:45:31 +0200

    update conanfiles

--- conanfile.py
+++ conanfile.py
@@ -51,7 +51,7 @@ class ArchimedesConan(ConanFile):
 		self.requires('glfw/3.4')
 		self.requires('glm/0.9.9.8', transitive_headers=True)
 		self.requires('spdlog/1.12.0')
-		self.requires('gtest/1.13.0')
+		self.requires('gtest/1.16.0')
 		self.requires('stb/cci.20230920', transitive_headers=True)
 		self.requires('draco/1.5.6', override=True)
 		self.requires('ctre/3.9.0', transitive_headers=True)
@@ -59,7 +59,7 @@ class ArchimedesConan(ConanFile):
 		self.requires('jsoncpp/1.9.6')
 
 		# Audio system
-		self.requires('openal-soft/1.23.1')
+		self.requires('openal-soft/1.24.3')
 		self.requires('libsndfile/1.2.2')
 
 		# Font atlas generator
@@ -72,7 +72,7 @@ class ArchimedesConan(ConanFile):
 		self.requires('nvrhi/2025.08.19')
 
 		# SPIRV (Shader compiler)
-		self.requires('shaderc/2023.6')
+		self.requires('shaderc/2025.3')
 
 		# msdf-atlas-gen
 		subprocess.run(['conan', 'install', '--requires=msdf-atlas-gen/1.3', '--build=missing'])
@@ -88,8 +88,8 @@ class ArchimedesConan(ConanFile):
 		cmake = CMake(self)
 		cmake.configure(variables={
 			'CMAKE_BUILD_TYPE': self.settings.build_type,
-			'CMAKE_C_COMPILER': os.environ.get("CC"),
-			'CMAKE_CXX_COMPILER': os.environ.get("CXX")
+			#'CMAKE_C_COMPILER': os.environ.get("CC"),
+			#'CMAKE_CXX_COMPILER': os.environ.get("CXX")
 		})
 		cmake.build()
 
