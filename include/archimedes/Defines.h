528ea5797186ef7e865feba6b2c5322475f88c95
Chris-plusplus <72704303+Chris-plusplus@users.noreply.github.com>
2026-07-20 22:24:43 +0200

    Create Defines.h

---
+++ include/archimedes/Defines.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#ifndef SPDLOG_USE_STD_FORMAT
+
+#define SPDLOG_USE_STD_FORMAT 1
+
+#endif
+
+#if defined(_MSC_VER) && defined(_WIN32)
+
+#define ARCHIMEDES_WINDOWS 1
+#define ARCHIMEDES_LINUX 0
+#define ARCHIMEDES_UNIX 0
+
+#elif defined(__GNUC__) && defined(__linux__)
+
+#define ARCHIMEDES_WINDOWS 0
+#define ARCHIMEDES_LINUX 1
+#define ARCHIMEDES_UNIX 1
+
+#endif
+
+#if defined(NDEBUG)
+
+#define ARCHIMEDES_DEBUG 0
+#define ARCHIMEDES_RELEASE 1
+
+#else
+
+#define ARCHIMEDES_DEBUG 1
+#define ARCHIMEDES_RELEASE 0
+
+#endif
