848e1954baf1a1065eef71654f87b9ef7f967763
Chris-plusplus <72704303+Chris-plusplus@users.noreply.github.com>
2026-07-21 20:19:03 +0200

    Update modules example

--- examples/modules/TestApp/main.cpp
+++ examples/modules/TestApp/main.cpp
@@ -1,9 +1,16 @@
 import archimedes;
 
+using arch::ecs::Domain;
 using arch::utils::UUID;
 
 #include <print>
 
 int main() {
-	std::println("dupa {}", UUID::random().toString());
+	Domain domain;
+	auto entity = domain.newEntity();
+	domain.addComponent<UUID>(entity, UUID::random());
+
+	for (auto [entity, uuid] : domain.view<UUID>().all()) {
+		std::println("{:v}: {}", entity, uuid.toString());
+	}
 }
