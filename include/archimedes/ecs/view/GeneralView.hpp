e25e956f98a8d99ec25591d786d5f5f942e728ae
Chris-plusplus <72704303+Chris-plusplus@users.noreply.github.com>
2026-07-25 13:18:50 +0200

    Update GeneralView.hpp

--- include/archimedes/ecs/view/GeneralView.hpp
+++ include/archimedes/ecs/view/GeneralView.hpp
@@ -173,7 +173,7 @@ template<class... Cs>
 auto VIEW_IE::_comps(Typelist<Cs...> wanted) noexcept {
 	constexpr auto cpoolsCast = wanted.transform<_details::cpoolCast>();
 
-	return std::views::all(*this) | std::views::transform([_cpools = _cpools](const Entity entity) {
+	return std::views::all(*this) | std::views::transform([_cpools = _cpools, &cpoolsCast](const Entity entity) {
 			   return std::tie(
 				   reinterpret_cast<getType<cpoolsCast.get(typelist<Cs...>.find(typelist<Cs>))>>(
 					   _cpools[includes.find(typelist<Cs>)]
@@ -209,7 +209,7 @@ template<class... Cs>
 auto VIEW_IE::_entityComps(Typelist<Cs...> wanted) noexcept {
 	constexpr auto cpoolsCast = wanted.transform<_details::cpoolCast>();
 
-	return std::views::all(*this) | std::views::transform([_cpools = _cpools](const Entity entity) {
+	return std::views::all(*this) | std::views::transform([_cpools = _cpools, &cpoolsCast](const Entity entity) {
 			   return std::tuple_cat(
 				   std::tuple(entity),
 				   std::tie(
