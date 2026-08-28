dc7b08ca49737be897d368a62375ee73605472e4
Chris-plusplus <72704303+Chris-plusplus@users.noreply.github.com>
2026-08-28 23:50:21 +0200

    Create DefaultValue.h

---
+++ include/archimedes/utils/DefaultValue.h
@@ -0,0 +1,11 @@
+#pragma once
+
+namespace arch::utils {
+
+/// @brief Helper struct for builders
+struct DefaultValue {};
+
+/// @brief Helper value for builders
+static inline constexpr DefaultValue defaultValue = {};
+
+} // namespace arch::utils
